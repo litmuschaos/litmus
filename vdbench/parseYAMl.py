c432f1cebffa2e3e1572ad57cc8df011becdaecc
deepakjayaprakash <deepakjpkodlekere@gmail.com>
2017-04-24 09:58:24 +0000

    Added output dir.. and generated paramFile

--- vdbench/parseYAMl.py
+++ vdbench/parseYAMl.py
@@ -1,26 +1,36 @@
 import yaml
 
 
-inputFile=open("input.yml",'r')
-dataMap=yaml.load(inputFile)
+def createParam():
+	inputFile=open("input.yml",'r')
+	dataMap=yaml.load(inputFile)
 
-outputFile=open('paramFile','w+')
-
-
-storageDefinition= "fsd="+dataMap['SDName']+","+"anchor="+dataMap['Anchor']+","+"depth="+dataMap['Depth']+","+"files="+dataMap['Files']+","+"size="+dataMap['Size']+","+"width="+dataMap['Width']+"\n"
-
-workDefinition= "fwd="+dataMap['WDName']+","+"fsd="+dataMap['SDForWD']+","+"rdpct="+dataMap['ReadPercentage']+","+"xfersize="+dataMap['TransferSize']+","+"fileio="+dataMap['FileIO']+","+"threads"+dataMap['Threads']+","+"fileselect="+dataMap['FileSelect']+"\n"
+	outputFile=open('paramFile','w+')
 
 
-runDefinition= "rd="+dataMap['RDName']+","+"fwd="+dataMap['WDForRD']+","+"elapsed="+dataMap['Duration']+","+"interval="+dataMap['Interval']+"\n"
+	storageDefinition= "fsd="+dataMap['SDName']+","+"anchor="+dataMap['Anchor']+","+"depth="+dataMap['Depth']+","+"files="+dataMap['Files']+","+"size="+dataMap['Size']+","+"width="+dataMap['Width']+"\n"
 
-outputFile.write(storageDefinition)
-outputFile.write(workDefinition)
-outputFile.write(runDefinition)
+	workDefinition= "fwd="+dataMap['WDName']+","+"fsd="+dataMap['SDForWD']+","+"rdpct="+dataMap['ReadPercentage']+","+"xfersize="+dataMap['TransferSize']+","+"fileio="+dataMap['FileIO']+","+"threads="+dataMap['Threads']+","+"fileselect="+dataMap['FileSelect']+"\n"
 
 
+	runDefinition= "rd="+dataMap['RDName']+","+"fwd="+dataMap['WDForRD']+","+"elapsed="+dataMap['Duration']+","+"interval="+dataMap['Interval']+","+"fwdrate="+dataMap['Fwdrate']+","+"format="+dataMap['Format']+"\n"
+
+	outputFile.write(storageDefinition)	
+	outputFile.write(workDefinition)
+	outputFile.write(runDefinition)
 
 
-outputFile.close()
-inputFile.close()
+
+
+	outputFile.close()
+	inputFile.close()
+	return
+
+
+def main():
+	createParam()
+	return 
+
+
+main()
 
