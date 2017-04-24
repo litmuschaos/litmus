10c09af3283e1185f2cfe97229b55ce574a77830
vagrant <vagrant@test-stg-dev>
2017-04-24 07:47:36 +0000

    Created intial template for param file generation# Please enter the commit message for your changes. Lines starting

--- vdbench/parseYAMl.py
+++ vdbench/parseYAMl.py
@@ -1,16 +1,26 @@
 import yaml
 
-#with open("input.yml", 'r') as stream:
- #   try:
-  #      print(yaml.load(stream))
-   # except yaml.YAMLError as exc:
-    #    print(exc)
+
+inputFile=open("input.yml",'r')
+dataMap=yaml.load(inputFile)
+
+outputFile=open('paramFile','w+')
 
 
-f=open("input.yml",'r')
-dataMap=yaml.load(f)
+storageDefinition= "fsd="+dataMap['SDName']+","+"anchor="+dataMap['Anchor']+","+"depth="+dataMap['Depth']+","+"files="+dataMap['Files']+","+"size="+dataMap['Size']+","+"width="+dataMap['Width']+"\n"
 
-print(dataMap['SDName'])
+workDefinition= "fwd="+dataMap['WDName']+","+"fsd="+dataMap['SDForWD']+","+"rdpct="+dataMap['ReadPercentage']+","+"xfersize="+dataMap['TransferSize']+","+"fileio="+dataMap['FileIO']+","+"threads"+dataMap['Threads']+","+"fileselect="+dataMap['FileSelect']+"\n"
 
-f.close()
+
+runDefinition= "rd="+dataMap['RDName']+","+"fwd="+dataMap['WDForRD']+","+"elapsed="+dataMap['Duration']+","+"interval="+dataMap['Interval']+"\n"
+
+outputFile.write(storageDefinition)
+outputFile.write(workDefinition)
+outputFile.write(runDefinition)
+
+
+
+
+outputFile.close()
+inputFile.close()
 
