49f10c7dbf327f6d240f736c10212df99f289887
Satyam Zode <satyamzode@gmail.com>
2017-07-06 12:36:52 +0530

    Add method to list pv's in the cluster
    This will add pvinfo.go which will list pv related
    information.

---
+++ k8s-client/pvinfo.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"k8s.io/client-go/kubernetes"
+	metav1 "k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/rest"
+)
+
+func main() {
+	// create in cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		pvs, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		for _, pv := range pvs.Items {
+			fmt.Printf("There's %s ", pv.GetName())
+		}
+	}
+}
