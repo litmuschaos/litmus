cdca0c406c60ddc85e4493ef51081101b7245325
Satyam Zode <satyamzode@gmail.com>
2017-07-07 16:24:03 +0530

    List pvc and pv's bound to pvcs

--- k8s-client/pvinfo.go
+++ k8s-client/pvinfo.go
@@ -21,13 +21,19 @@ func main() {
 		panic(err.Error())
 	}
 	for {
+
 		pvs, err := clientset.CoreV1().PersistentVolumes().List(metav1.ListOptions{})
+		pvcs, err := clientset.CoreV1().PersistentVolumeClaims("").List(metav1.ListOptions{})
+
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Println("Getting PV's available")
 		for _, pv := range pvs.Items {
-			fmt.Printf("There's %s ", pv.GetName())
+			fmt.Printf("Persistent Volume:  %s ", pv.GetName())
+			time.Sleep(10 * time.Second)
+		}
+		for _, pvc := range pvcs.Items {
+			fmt.Printf("Persistent Volume Claim: %s , Volume Name: %s", pvc.GetName(), pvc.Spec.VolumeName)
 			time.Sleep(10 * time.Second)
 		}
 	}
