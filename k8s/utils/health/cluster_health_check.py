e3f18628ccbf50ed1f9e0780f861caea4126724d
harshvkarn <harshvkarn54@gmail.com>
2018-08-11 01:35:52 +0530

    Add the copyright header and Info/functionality
    - Add copyright header to the python script, `cluster_health_check.py`
    - Add Info/functionality/need/usage as the script's comment better
      readablilty
    
    Signed-off-by: harshvkarn <harshvkarn54@gmail.com>

--- k8s/utils/health/cluster_health_check.py
+++ k8s/utils/health/cluster_health_check.py
@@ -1,3 +1,24 @@
+# Copyright 2018 The OpenEBS Authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+#
+# Info:
+# This Python script can be used to check the cluster readiness over multiple Clouds
+# providers like GCE, GKE and AWS with any number of nodes. Script does a series of 
+# api calls to the kubernetes cluster, by utilising the Python client library for kubernetes.
+# Script terminates successfully once cluster is up or else fails after a T/O period of 900 seconds
+# To run the script: `python cluster_health_check.py --nodes 4`
+
 from kubernetes import client, config
 import time
 import argparse
