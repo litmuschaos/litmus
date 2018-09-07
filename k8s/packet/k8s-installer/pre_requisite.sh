600adc04b869dd160a12e4c7b2e49891ac785baf
Chandan Kumar <chandan.kr404@gmail.com>
2018-09-07 15:25:49 +0530

    Set network-cidr as a vars
    - Network cidr treated as a vars because, user can change according to
    their usages
    
    Signed-off-by: Chandan Kumar <chandan.kr404@gmail.com>

--- k8s/packet/k8s-installer/pre_requisite.sh
+++ k8s/packet/k8s-installer/pre_requisite.sh
@@ -10,14 +10,14 @@ master() {
     sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
     systemctl daemon-reload
     systemctl restart kubelet
-    kubeadm init --apiserver-advertise-address=$1 --apiserver-cert-extra-sans=10.0.2.15 --pod-network-cidr 10.1.0.0/16
+    kubeadm init --apiserver-advertise-address=$1 --apiserver-cert-extra-sans=10.0.2.15 --pod-network-cidr=$2
     mkdir -p $HOME/.kube
     cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     chown $(id -u):$(id -g) $HOME/.kube/config
     kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 }
 
-if [ "$2" = "master" ]
+if [ "$3" = "master" ]
 then
   master
 fi
