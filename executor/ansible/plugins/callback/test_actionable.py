a8bb1134c283aa31728fc650505b4a09d4aaa00b
Sandeep Choudhary <sandeep@letstranzact.com>
2018-10-27 00:32:15 +0530

    Write test case for CallbackModule
    Signed-off-by: Sandeep Choudhary <sandeep@letstranzact.com>

--- executor/ansible/plugins/callback/test_actionable.py
+++ executor/ansible/plugins/callback/test_actionable.py
@@ -1,27 +1,52 @@
 import unittest
+from ansible.plugins.callback.actionable import CallbackModule
+
 
 class TestCallbackModule(unittest.TestCase):
     
     def setUp(self):
         pass
 
-    def test_v2_playbook_on_handler_task_start(self, task):
-        ''
+    def test_v2_playbook_on_handler_task_start(self):
+        cb = CallbackModule()
+        task = ''
+        banner_text = cb.v2_playbook_on_handler_task_start(task)
+        self.assertEquals(banner_text, '')
+        self.assertEquals(cb.shown_title, True)
 
-    def test_v2_playbook_on_task_start(self, task, is_conditional):
-        ''
-    
+    def test_v2_playbook_on_task_start(self):
+        cb = CallbackModule()
+        task = ''
+        is_conditional = ''
+        cb.v2_playbook_on_task_start(task, is_conditional)
+
+        self.assertEquals(cb.last_task, task)
+        self.assertEquals(cb.shown_title, True)
+
     def test_display_task_banner(self):
-        ''
+        cb = CallbackModule()
+        task = ''
+        cb.display_task_banner()
+        self.assertEquals(cb.last_task, task)
+        self.assertEquals(cb.shown_title, True)
 
-    def test_v2_runner_on_failed(self, result, ignore_errors=False):
-        ''
+    def test_v2_runner_on_failed(self):
+        cb = CallbackModule()
+        result = ''
+        ignore_errors = ''
+        cb.v2_runner_on_failed(result, ignore_errors)
+        # self.assertEquals('', '')
 
-    def test_v2_runner_on_ok(self, result):
-        ''
+    def test_v2_runner_on_ok(self):
+        cb = CallbackModule()
+        result = {'changed': True}
+        cb.v2_runner_on_ok(result)
+        # self.assertEquals('', '')
 
     def test_v2_runner_on_unreachable(self, result):
-        ''
+        cb = CallbackModule()
+        cb.v2_runner_on_unreachable()
+        # self.assertEquals('', '')
 
     def test_v2_runner_on_skipped(self, result):
         pass
@@ -30,16 +55,29 @@ class TestCallbackModule(unittest.TestCase):
         pass
 
     def test_v2_runner_item_on_ok(self, result):
-        ''
+        cb = CallbackModule()
+        result = {'changed': True}
+        cb.v2_runner_item_on_failed(result)
+        # self.assertEquals('', '')
+
+        result = {'changed': False}
+        cb.v2_runner_item_on_failed(result)
+        # self.assertEquals('', '')
 
     def test_v2_runner_item_on_skipped(self, result):
         pass
 
     def test_v2_runner_item_on_failed(self, result):
-        ''
+        cb = CallbackModule()
+        cb.v2_runner_item_on_failed()
+        # self.assertEquals('', '')
 
     def test_v2_runner_retry(self, result):
-        ''
+        cb = CallbackModule()
+        result = ''
+        cb.v2_runner_retry(result)
+        # self.assertEquals('', '')
+
 
 if __name__ == '__main__':
     unittest.main()
