35623e9514cbea776a24f3fc757fbe7b0c71be96
Sudarshan <sudarshan.darga@cloudbyte.com>
2018-11-16 15:01:32 +0530

    Fetch istgt process id dynimically
    Signed-off-by: Sudarshan <sudarshan.darga@cloudbyte.com>

--- apps/prometheus/functional/memcheck/test-mem.py
+++ apps/prometheus/functional/memcheck/test-mem.py
@@ -7,17 +7,22 @@ from __future__ import division
 import subprocess
 import time, os, sys
 list = []
-target_name = sys.argv[1]
+targetname = sys.argv[1]
 namespace = sys.argv[2]
-used_mem_process = "kubectl exec %s -n %s -- pmap -x 8 | awk ''/total'/ {print $3}'" %(target_name,namespace)
+getpid = "kubectl exec %s -n %s -- pidof /usr/local/bin/istgt" %(targetname,namespace)
+print (getpid)
+out = subprocess.Popen(getpid,stdout=subprocess.PIPE,shell=True)
+pid = out.communicate()
+istgtpid = int(pid[0])
+used_mem_process = "kubectl exec %s -n %s -- pmap -x %s | awk ''/total'/ {print $3}'" %(targetname,namespace,istgtpid)
 n = 10
 count = 0
-#Obtaining memory consumed by longhorn process from the cntroller pod.
+#Obtaining memory consumed by cstor-istgt process from the target pod.
 while count < n:
     count = count + 1
     out = subprocess.Popen(used_mem_process,stdout=subprocess.PIPE,shell=True)
     used_mem = out.communicate()
-    mem_in_mb = int(float(used_mem[0]))/1024
+    mem_in_mb = int(used_mem[0])/1024
     print (mem_in_mb), "MB"
     if mem_in_mb < 500:
         time.sleep(20)
@@ -27,7 +32,7 @@ while count < n:
         quit()
     list.append(mem_in_mb)
 print (list)
-# A watermark of 800MB(re-calibrated based on results oberved from latest sanity run) 
+# A watermark of 500MB(re-calibrated based on results oberved from latest sanity run) 
 # profile chosen in this test
 # TODO: Identify better mem consumption strategies
 if all(i <= 500 for i in list):
