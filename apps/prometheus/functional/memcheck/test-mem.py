1fa1ab6e3ac4734373aa73ee27ce0d6e6686c37a
rampreeth <ethirajrampreeth@gmail.com>
2019-03-31 18:31:27 +0530

    add process name and threshold as parameters to the script
    Signed-off-by: rampreeth <ethirajrampreeth@gmail.com>

--- apps/prometheus/functional/memcheck/test-mem.py
+++ apps/prometheus/functional/memcheck/test-mem.py
@@ -19,8 +19,9 @@ def compute_mem_func(args):
     """ this func computes the memory consumption over 10 iterations and returns the result """
     namespace = args.namespace
     targetname = args.target_name
+    threshold = args.mem_threshold
     mem_list = []
-    getpid = "kubectl exec %s -n %s -- pidof /usr/local/bin/istgt" %(targetname, namespace)
+    getpid = "kubectl exec %s -n %s -- pidof %s" %(targetname, namespace, args.process_name)
     print getpid
     pid = subprocess_wrapper(getpid)
     istgtpid = int(pid[0])
@@ -30,11 +31,11 @@ def compute_mem_func(args):
         used_mem = subprocess_wrapper(used_mem_process)
         mem_in_mb = int(float(used_mem[0]))/1024
         print (mem_in_mb), "MB"
-        if mem_in_mb < 500:
+        if mem_in_mb < threshold:
             time.sleep(20)
         else:
             print "Fail"
-            sys.exit("Memory consumption > 500 MB, exiting")
+            sys.exit("Memory consumption > {} MB, exiting".format(threshold))
         mem_list.append(mem_in_mb)
     return mem_list
 
@@ -45,6 +46,9 @@ def main():
     parser.add_argument('-t', '--target_name', required=True, help='The target pod')
     parser.add_argument('-n', '--namespace', required=True, help="The namespace where the \
     target pod is running")
+    parser.add_argument('-m', '--mem_threshold', required=True, help='The mem threshold')
+    parser.add_argument('-p', '--process_name', required=True, help='The process whose mem \
+    consumption has to be checked')
     args = parser.parse_args()
     compute_mem_list = compute_mem_func(args)
     # A watermark of 500MB(re-calibrated based on results oberved from latest sanity run)
@@ -57,4 +61,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
