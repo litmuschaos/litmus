aa057b920f3ace7105c0909c6aafd8b01624d840
Saranya-jena <Saranya.jena@mayadata.io>
2020-06-26 10:25:33 +0530

    Added Sidebar (#1595)
    * Added sidebar
    
    Signed-off-by: Saranya-jena <saranyajena29@yahoo.com>
    
    * Minor corrections
    
    Signed-off-by: Saranya-jena <saranyajena29@yahoo.com>
    
    * minor css changes
    
    Signed-off-by: Saranya-jena <saranyajena29@yahoo.com>

--- litmus-portal/frontend/src/components/SideBar/styles.ts
+++ litmus-portal/frontend/src/components/SideBar/styles.ts
@@ -7,9 +7,69 @@ export const useStyles = makeStyles((theme: Theme) => ({
 	},
 	drawerPaper: {
 		//TODO: remove padding top
-		paddingTop: theme.spacing(8),
+	
 
 		width: "100%",
 		position: "relative",
 	},
+	litmusDiv: {
+		display: "flex",
+		flexDirection: "row",
+		marginTop: theme.spacing(3.5),
+		marginLeft: theme.spacing(4)
+	},
+	logo: {
+		left: 35,
+		
+		
+	},
+	litmusHome: {
+	
+		width: 65,
+		height: 30,
+		fontWeight: "normal",
+		fontFamily: "Ubuntu Condensed",
+		fontStyle: "normal",
+		fontSize: 26,
+		left: 81,
+		top: 24,
+		marginLeft: theme.spacing(1.75),
+		color: "#000000"	
+	
+	},
+	drawerListItem: {
+		display: "flex",
+		height: 51,
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "center",
+		"&:hover": {
+			backgroundColor: "#858CDD",
+			color: theme.palette.getContrastText(
+			theme.palette.primary.contrastText)
+		},
+	},
+
+	listIcon: {
+		
+		paddingLeft: theme.spacing(2),
+		alignSelf: "center"
+	},
+	listText: {
+	
+		marginLeft: theme.spacing(0),
+		fontSize: 16,
+		
+		fontFamily: "Ubuntu",
+		fontWeight: "normal",
+		fontStyle: "normal"
+		
+	},
+	drawerList: {
+		
+		marginTop: theme.spacing(8),
+		height: 267,
+		
+	},
+		
 }));
