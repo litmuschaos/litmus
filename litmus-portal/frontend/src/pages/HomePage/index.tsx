4b547f187beee9da0e1552d5933ec89d7357e7a5
Sayan Mondal <sayan.mondal@mayadata.io>
2020-07-03 23:18:34 +0530

    updated(Community Page): Created Basic Community Layout and updated InfoFilled Component + Modified formatCount Utility
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- litmus-portal/frontend/src/pages/HomePage/index.tsx
+++ litmus-portal/frontend/src/pages/HomePage/index.tsx
@@ -2,7 +2,7 @@ import { Card, CardActionArea, Typography } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import CustomInfoFilled from '../../components/InfoFilled/index';
+import InfoFilled from '../../components/InfoFilled/index';
 import QuickActionCard from '../../components/QuickActionCard';
 import Scaffold from '../../containers/layouts/Scaffold';
 import useStyles from './style';
@@ -75,24 +75,28 @@ const HomePage = () => {
                 <strong>How busy Litmus Project is?</strong>
               </Typography>
               <div className={classes.cardDiv}>
-                <CustomInfoFilled
+                <InfoFilled
                   color="#109B67"
-                  value="11.2K"
+                  value={11.2}
+                  plus
                   statType="Operator Installed"
                 />
-                <CustomInfoFilled
+                <InfoFilled
                   color="#858CDD"
-                  value="29+"
+                  value={29}
+                  plus
                   statType="Total Experiments"
                 />
-                <CustomInfoFilled
+                <InfoFilled
                   color="#F6B92B"
-                  value="60K+"
+                  value={60}
+                  plus
                   statType="Total Runs Experiments"
                 />
-                <CustomInfoFilled
+                <InfoFilled
                   color="#BA3B34"
-                  value="800+"
+                  value={800}
+                  plus
                   statType="Github Stars"
                 />
               </div>
