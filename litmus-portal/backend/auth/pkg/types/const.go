707d5b15379f92a37647509826fa3c3a605d5e59
Sanjay Nathani <50315249+Sanjay1611@users.noreply.github.com>
2020-07-06 17:19:25 +0530

    initial commit building auth server (#1653)
    Signed-off-by: Sanjay Nathani <sanjay.nathani@mayadata.io>

---
+++ litmus-portal/backend/auth/pkg/types/const.go
@@ -0,0 +1,16 @@
+package types
+
+import "time"
+
+// define the type of authorization request
+const (
+	RedirectURI                string = "http://localhost:3000/"
+	DefaultDBServerURL         string = "mongodb://127.0.0.1:27017"
+	DefaultAuthDB              string = "auth"
+	DefaultLocalAuthCollection string = "user"
+	DefaultAPISecret           string = "litmus-portal@123"
+	DefaultUserName            string = "admin"
+	DefaultUserPassword        string = "litmus"
+	PasswordEncryptionCost     int    = 15
+	TimeFormat                 string = time.RFC1123Z
+)
