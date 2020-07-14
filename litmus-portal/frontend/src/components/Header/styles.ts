ee1b9d493bc4f3f8c3c284478842405d976488d6
ishangupta-ds <ishan.gupta@mayadata.io>
2020-07-14 12:46:37 +0530

    Added header and related components.
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

--- litmus-portal/frontend/src/components/Header/styles.ts
+++ litmus-portal/frontend/src/components/Header/styles.ts
@@ -1,10 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
   appBar: {
     width: '100%',
     height: '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+  breadCrumbsGrid: {
+    marginTop: theme.spacing(2),
+    paddingLeft: theme.spacing(4),
+  },
+  notificationSection: {
+    marginTop: theme.spacing(1),
   },
 }));
 
