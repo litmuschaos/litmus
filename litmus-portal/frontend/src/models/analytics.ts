f0178f47a212ab9c1e702a88a2bfe60ae938704d
ishangupta-ds <ishan.gupta@mayadata.io>
2020-07-14 09:16:00 +0530

    Removed bugs.
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

--- litmus-portal/frontend/src/models/analytics.ts
+++ litmus-portal/frontend/src/models/analytics.ts
@@ -1,91 +1,46 @@
-import createReducer from './createReducer';
-import {
-  CommunityData,
-  AnalyticsAction,
-  AnalyticsActions,
-  GeoCity,
-  SeriesData,
-} from '../../models';
+export enum AnalyticsActions {
+  LOAD_COMMUNITY_ANALYTICS = 'LOAD_COMMUNITY_ANALYTICS',
+}
 
-const initialState: CommunityData = {
-  github: { stars: '', experimentsCount: '' },
-  google: {
-    totalRuns: '',
-    operatorInstalls: '',
-    geoCity: [],
-    geoCountry: [],
-    dailyExperimentData: [],
-    dailyOperatorData: [],
-    monthlyExperimentData: [],
-    monthlyOperatorData: [],
-  },
-};
+export interface Github {
+  stars: string;
+  experimentsCount: string;
+}
 
-export const communityData = createReducer<CommunityData>(initialState, {
-  [AnalyticsActions.LOAD_COMMUNITY_ANALYTICS](
-    state: CommunityData,
-    action: AnalyticsAction
-  ) {
-    const data = action.payload;
-    const geoCity: GeoCity[] = [];
-    data.google.geoCity.forEach((c: any) => {
-      geoCity.push({
-        name: c[0],
-        latitude: c[1],
-        longitude: c[2],
-        count: c[3],
-      });
-    });
+export interface GeoCity {
+  name: string;
+  latitude: string;
+  longitude: string;
+  count: string;
+}
 
-    const geoCountry: string[][] = data.google.geoCountry;
+export interface SeriesData {
+  date: string;
+  count: string;
+}
 
-    const dailyExperimentData: SeriesData[] = [];
-    data.google.dailyExperimentData.forEach((c: any) => {
-      dailyExperimentData.push({
-        date: c[0],
-        count: c[1],
-      });
-    });
+export interface Google {
+  totalRuns: string;
+  operatorInstalls: string;
+  geoCity: GeoCity[];
+  geoCountry: string[][];
+  dailyOperatorData: SeriesData[];
+  dailyExperimentData: SeriesData[];
+  monthlyOperatorData: SeriesData[];
+  monthlyExperimentData: SeriesData[];
+}
 
-    const dailyOperatorData: SeriesData[] = [];
-    data.google.dailyOperatorData.forEach((c: any) => {
-      dailyOperatorData.push({
-        date: c[0],
-        count: c[1],
-      });
-    });
+export interface CommunityData {
+  github: Github;
+  google: Google;
+}
 
-    const monthlyExperimentData: SeriesData[] = [];
-    data.google.monthlyExperimentData.forEach((c: any) => {
-      monthlyExperimentData.push({
-        date: c[0],
-        count: c[1],
-      });
-    });
+interface AnalyticsActionType<T, P> {
+  type: T;
+  payload: P;
+}
 
-    const monthlyOperatorData: SeriesData[] = [];
-    data.google.monthlyOperatorData.forEach((c: any) => {
-      monthlyOperatorData.push({
-        date: c[0],
-        count: c[1],
-      });
-    });
-
-    return {
-      ...state,
-      github: data.github,
-      google: {
-        totalRuns: data.google.totalRuns,
-        operatorInstalls: data.google.operatorInstalls,
-        geoCountry,
-        geoCity,
-        dailyExperimentData,
-        dailyOperatorData,
-        monthlyExperimentData,
-        monthlyOperatorData,
-      },
-    };
-  },
-});
-
-export default communityData;
+export type AnalyticsAction = AnalyticsActionType<
+  typeof AnalyticsActions.LOAD_COMMUNITY_ANALYTICS,
+  CommunityData
+>;
