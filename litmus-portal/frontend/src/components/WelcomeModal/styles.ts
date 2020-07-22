8a0b6a73f150feb6c749bf47e9dcc83082776306
Sayan Mondal <sayan.mondal@mayadata.io>
2020-07-22 17:35:37 +0530

    Updated Welcome Modal
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- litmus-portal/frontend/src/components/WelcomeModal/styles.ts
+++ litmus-portal/frontend/src/components/WelcomeModal/styles.ts
@@ -1,95 +1,81 @@
 import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      // width: 350,
-      outline: 'none',
-      marginTop: 30,
-    },
-  },
+  /* CSS for Index */
   rootContainer: {
-    marginLeft: '21rem',
-    marginRight: '21rem',
-    marginTop: '7.8rem',
-    marginBottom: '7.8rem',
-    paddingBottom: '6rem',
+    margin: '3rem auto',
+    width: '60%',
+    height: '85%',
     background: theme.palette.common.white,
     borderRadius: 3,
     textAlign: 'center',
     outline: 'none',
   },
+
+  /* CSS for Modal Page Component */
+  buttonOutline: {
+    minWidth: theme.spacing(6),
+    height: '2.2rem',
+    border: '0.0625rem solid',
+    borderColor: theme.palette.secondary.dark,
+    marginRight: theme.spacing(2),
+  },
   heading: {
-    fontfamily: 'Ubuntu',
-    fontstyle: 'normal',
-    fontweight: 'normal',
-    fontSize: '40px',
+    fontSize: '2rem',
     textalign: 'center',
-    marginTop: '2rem',
+    marginTop: theme.spacing(3),
     color: theme.palette.common.black,
   },
   infoHeading: {
-    fontfamily: 'Ubuntu',
-    fontstyle: 'normal',
-    fontweight: 'normal',
-    fontsize: '16px',
-    lineheight: '170%',
+    fontfamily: theme.typography.fontFamily,
+    fontsize: '2rem',
     textalign: 'center',
     color: theme.palette.common.black,
-    marginTop: '2.5rem',
-    marginBottom: 15,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+  },
+  insideModal: {
+    marginBottom: theme.spacing(10),
   },
   mark: {
-    marginTop: 80,
+    marginTop: theme.spacing(6),
     textAlign: 'center',
   },
-  experimentname: {
-    textAlign: 'center',
-    marginBottom: 30,
-  },
-  stepper: {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-  },
+
   /* CSS for input and password field */
   inputArea: {
-    marginTop: theme.spacing(3.76),
-    display: 'flex',
-    textAlign: 'center',
-    marginLeft: theme.spacing(37.2),
-    marginRight: theme.spacing(37.2),
-    textDecoration: 'none',
-    flexDirection: 'column',
-    paddingLeft: theme.spacing(2),
-    paddingBottom: theme.spacing(2.2),
-    borderRadius: 3,
-    border: '0.0625rem solid $(theme.palette.secondary.dark)',
-    borderLeft: '0.1875rem solid $(theme.palette.secondary.dark)',
+    width: '60%',
+    margin: '1rem auto',
+  },
+  passwordArea: {
+    width: '60%',
+    margin: '0 auto',
+    marginTop: '-1rem',
+    marginBottom: '1rem',
   },
   inputDiv: {
     marginTop: theme.spacing(6.26),
     textAlign: 'center',
     marginLeft: 30,
   },
-  /* CSS for stpper */
-  rootStepper: {
-    color: theme.palette.common.white,
+
+  /* CSS for Password Div in Stepper Component */
+  passwordSetterDiv: {
     display: 'flex',
-    height: 22,
-    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: theme.spacing(-3),
   },
-  activeStepper: {
-    color: theme.palette.secondary.dark,
+
+  /* CSS for stepper */
+  stepper: {
+    position: 'absolute',
+    left: '50%',
+    transform: 'translate(-50%, 0)',
+    bottom: '13%',
   },
-  circleStepper: {
-    width: 8,
-    height: 8,
-    borderRadius: '50%',
-    backgroundColor: 'currentColor',
+  buttonDiv: {
+    position: 'relative',
+    marginBottom: theme.spacing(1),
   },
 }));
 export default useStyles;
