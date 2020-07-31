d922e84fe07bd721ea4791230c624505c480ea84
Sayan Mondal <sayan.mondal@mayadata.io>
2020-07-31 16:34:07 +0530

    Removed ChartGroup
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- litmus-portal/frontend/src/components/WorkflowCluster/WorkflowChart.tsx
+++ litmus-portal/frontend/src/components/WorkflowCluster/WorkflowChart.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Chart from '../Charts';
 import useStyles from './styles';
 
 const WorkflowChart: React.FC = () => {
   const classes = useStyles();
   return (
-    <div className={classes.rootcontainer}>
-      <Chart />
-    </div>
+    <div className={classes.rootcontainer}>{/* Chart Group Goes Here */}</div>
   );
 };
 
