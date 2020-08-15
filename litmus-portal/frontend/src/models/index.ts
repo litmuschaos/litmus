eada55d3730523c928d846306d359b32c10c4318
ksatchit <karthik.s@mayadata.io>
2020-08-15 16:07:23 +0000

    Revert "moving commits from litmus-portal  to master (#1872)"
    This reverts commit eaa644fea4500dc4d6e53b7c795b2078842e46a5.

--- litmus-portal/frontend/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-import { AnalyticsAction } from './analytics';
-import { UserAction } from './user';
-import { WorkflowAction } from './workflow';
-
-export type Action = UserAction | AnalyticsAction | WorkflowAction;
--- litmus-portal/frontend/src/utils/actions.ts
+++
@@ -1,9 +0,0 @@
-const requestActions = (name: string) => {
-  return {
-    REQUEST: name,
-    SUCCESS: `${name}_SUCCESS`,
-    FAIL: `${name}_FAIL`,
-  };
-};
-
-export default requestActions;
