9543a5ae19631204fa66d1416b50168f4ac41d6a
ishangupta-ds <ishan.gupta@mayadata.io>
2020-09-13 12:25:31 +0530

    Updating test case for quick actions card.
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

--- litmus-portal/frontend/cypress/component/QuickActionCard.spec.tsx
+++ litmus-portal/frontend/cypress/component/QuickActionCard.spec.tsx
@@ -1,35 +1,49 @@
 /// <reference types="Cypress" />
 import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { mount } from 'cypress-react-unit-test';
-import { BrowserRouter } from 'react-router-dom';
 import QuickActionCard from '../../src/components/QuickActionCard';
-
-describe('QuickActionCard Testing', () => {
-  it('Component is Visible', () => {
-    mount(
+import configureStore from '../../src/redux/configureStore';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+const { store } = configureStore();
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+});
+const wrapper = (
+  <ApolloProvider client={client}>
+    <Provider store={store}>
       <BrowserRouter>
         <QuickActionCard />
       </BrowserRouter>
-    );
+    </Provider>
+  </ApolloProvider>
+);
+describe('QuickActionCard Testing', () => {
+  it('Component is Visible', () => {
+    mount(wrapper);
   });
-  it('Connect a cluster', () =>
+  it('Invite a team member', () =>
     cy
       .get(':nth-child(1) > .makeStyles-listItem-2')
       .should('be.visible')
-      .click());
-  it('Invite a team member', () =>
+      .click()
+      .location()
+      .should((loc) => {
+        expect(loc.href).to.include('/settings');
+      }));
+  it('Take a quick survey and check the survey link', () =>
     cy
       .get(':nth-child(2) > .makeStyles-listItem-2')
       .should('be.visible')
-      .click());
-  it('Take a quick survey', () =>
+      .should('have.attr', 'href', 'https://forms.gle/qMuVphRyEWCFqjD56'));
+  it('Read Litmus Docs and check the docs link', () =>
     cy
       .get(':nth-child(3) > .makeStyles-listItem-2')
       .should('be.visible')
-      .click());
-  it('Read Litmus Docs', () =>
-    cy
-      .get(':nth-child(4) > .makeStyles-listItem-2')
-      .should('be.visible')
-      .click());
+      .should(
+        'have.attr',
+        'href',
+        'https://docs.litmuschaos.io/docs/getstarted/'
+      ));
 });
