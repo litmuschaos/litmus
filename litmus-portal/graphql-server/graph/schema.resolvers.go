9b86ea12313a09df7faa239d6722a6bb066fdd1e
Raj Babu Das <raj.das@mayadata.io>
2020-09-28 17:25:40 +0530

    Removing getWorkflows query
    Signed-off-by: Raj Babu Das <raj.das@mayadata.io>

--- litmus-portal/graphql-server/graph/schema.resolvers.go
+++ litmus-portal/graphql-server/graph/schema.resolvers.go
@@ -6,7 +6,6 @@ package graph
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"strconv"
 	"time"
@@ -215,13 +214,3 @@ func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subsc
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *queryResolver) GetWorkflow(ctx context.Context, workflowID string) (*model.Workflow, error) {
-	panic(fmt.Errorf("not implemented"))
-}
