7704d219b7321ff2aef688cbf500ac5c34650f98
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2020-10-14 10:32:51 +0530

    Update index.tsx
    Signed-off-by: Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>

--- litmus-portal/frontend/src/components/ProgressBar/AnalyticsLinearProgressBar/index.tsx
+++ litmus-portal/frontend/src/components/ProgressBar/AnalyticsLinearProgressBar/index.tsx
@@ -1,31 +1,26 @@
-import { Line } from 'rc-progress';
-import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-
-interface LinearProgressBarProps {
-  value: number;
-  maxValue: number;
-  isInTable: boolean;
-}
-
-const AnalyticsLinearProgressBar: React.FC<LinearProgressBarProps> = ({
-  value,
-  maxValue,
-  isInTable,
-}) => {
-  const width: number = 2;
-  const resultValue = ((value as number) / (maxValue as number)) * 100;
-
-  const { palette } = useTheme();
-
+import React from "react";
+import LinearProgress from "@material-ui/core/LinearProgress";
+const MIN = 100;
+const MAX = 200;
+const normalize = value => ((value - MIN) * 100) / (MAX - MIN);
+export default function App() {
+  const [progress, setProgress] = React.useState(MIN);
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(oldProgress => {
+        if (oldProgress === MAX) {
+          return 0;
+        }
+        return Math.min(oldProgress + 15, MAX);
+      });
+    }, 500);
+  return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
-    <Line
-      percent={resultValue}
-      strokeWidth={width}
-      trailWidth={width}
-      strokeColor={isInTable ? palette.primary.dark : palette.secondary.dark}
-    />
+    <div>
+      <LinearProgress variant="determinate" value={normalize(progress)} />
+    </div>
   );
-};
-
-export default AnalyticsLinearProgressBar;
+}
