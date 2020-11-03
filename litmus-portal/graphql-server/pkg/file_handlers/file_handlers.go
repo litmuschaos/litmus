65725828850a6968d80a68257dd39d078ff0c1b8
Raj Babu Das <raj.das@mayadata.io>
2020-11-04 00:59:49 +0530

    Adding self connection code
    Signed-off-by: Raj Babu Das <raj.das@mayadata.io>

--- litmus-portal/graphql-server/pkg/file_handlers/file_handlers.go
+++ litmus-portal/graphql-server/pkg/file_handlers/file_handlers.go
@@ -30,54 +30,77 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 	var (
 		vars           = mux.Vars(r)
 		token          = vars["key"]
+	)
+	log.Print("here")
+	log.Print(token)
+
+	response, statusCode, err := GetManifest(token)
+	if err != nil{
+		log.Print("error", err)
+		utils.WriteHeaders(&w, statusCode)
+	}
+	//log.Print(response)
+	log.Print("here")
+
+	utils.WriteHeaders(&w, statusCode)
+	w.Write(response)
+}
+
+func GetManifest(token string) ([]byte, int, error) {
+	var (
 		portalEndpoint string
 	)
 
 	id, err := cluster.ClusterValidateJWT(token)
 	if err != nil {
 		log.Print("ERROR", err)
-		utils.WriteHeaders(&w, 404)
-		return
+		return nil, 404, err
 	}
 
 	reqCluster, err := database.GetCluster(id)
 	if err != nil {
 		log.Print("ERROR", err)
-		utils.WriteHeaders(&w, 500)
-		return
+		return nil, 500, err
 	}
 
 	if os.Getenv("PORTAL_SCOPE") == "cluster" {
 		portalEndpoint, err = k8s.GetPortalEndpoint()
 		if err != nil {
-			log.Print(err)
+			return nil, 500, err
 		}
 	} else if os.Getenv("PORTAL_SCOPE") == "namespace" {
 		portalEndpoint = os.Getenv("PORTAL_ENDPOINT")
 	}
 
 	subscriberConfiguration.GQLServerURI = portalEndpoint + "/query"
-
+	log.Print(reqCluster)
 	if !reqCluster.IsRegistered {
 		var respData []byte
 
 		if reqCluster.AgentScope == "cluster" {
 			respData, err = utils.ManifestParser(reqCluster, "manifests/cluster-subscriber.yml", subscriberConfiguration)
+				log.Print("found it")
+
+			//log.Print(respData)
+			//log.Print(err)
+			//if err != nil {
+			//	log.Print("found it")
+			//	return nil, 500, err
+			//}
 		} else if reqCluster.AgentScope == "namespace" {
 			respData, err = utils.ManifestParser(reqCluster, "manifests/namespace-subscriber.yml", subscriberConfiguration)
+			if err != nil {
+				return nil, 500, err
+			}
 		} else {
 			log.Print("ERROR- AGENT SCOPE NOT SELECTED!")
 		}
 
-		if err != nil {
-			log.Print("ERROR", err)
-			utils.WriteHeaders(&w, 500)
-			return
-		}
-		utils.WriteHeaders(&w, 200)
-		w.Write(respData)
-		return
+		//utils.WriteHeaders(&w, 200)
+		//w.Write(respData)
+		return respData, 200, nil
+	} else {
+		return []byte("Cluster is already registered"), 200, nil
 	}
 
-	utils.WriteHeaders(&w, 404)
 }
