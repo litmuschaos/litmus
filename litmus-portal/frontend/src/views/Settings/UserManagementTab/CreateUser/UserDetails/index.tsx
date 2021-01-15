540a29f20aed304a9a724bd3b75c63ee9bea6981
Sayan Mondal <sayan.mondal@mayadata.io>
2021-01-15 16:00:25 +0530

    InputField BugFix in Settings Page
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- litmus-portal/frontend/src/views/Settings/UserManagementTab/CreateUser/UserDetails/index.tsx
+++ litmus-portal/frontend/src/views/Settings/UserManagementTab/CreateUser/UserDetails/index.tsx
@@ -1,7 +1,7 @@
 import { Avatar, Button, Typography } from '@material-ui/core';
+import { InputField } from 'kubera-ui';
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import InputFieldOutline from '../../../../../components/InputFieldOutline';
 import Unimodal from '../../../../../containers/layouts/Unimodal';
 import {
   validateEmail,
@@ -79,7 +79,7 @@ const UserDetails: React.FC<PersonalDetailsProps> = ({
           {/* Fields for details including Full name, email, username */}
           <div className={classes.details1}>
             <div data-cy="InputName">
-              <InputFieldOutline
+              <InputField
                 required
                 helperText={
                   validateStartEmptySpacing(nameValue)
@@ -88,15 +88,18 @@ const UserDetails: React.FC<PersonalDetailsProps> = ({
                 }
                 value={nameValue}
                 disabled={nameIsDisabled}
-                handleChange={handleNameChange}
-                validationError={validateStartEmptySpacing(nameValue)}
+                onChange={handleNameChange}
+                variant={
+                  validateStartEmptySpacing(nameValue) ? 'error' : 'primary'
+                }
                 label={t(
                   'settings.userManagementTab.createUser.userDetails.label.fullName'
                 )}
               />
             </div>
+            <div style={{ width: '2rem' }} />
             <div data-cy="InputEmail">
-              <InputFieldOutline
+              <InputField
                 required
                 helperText={
                   validateEmail(emailValue) ? 'Should be a valid email' : ''
@@ -104,20 +107,21 @@ const UserDetails: React.FC<PersonalDetailsProps> = ({
                 type="email"
                 value={emailValue}
                 disabled={emailIsDisabled}
-                handleChange={handleEmailChange}
-                validationError={validateEmail(emailValue)}
+                onChange={handleEmailChange}
+                variant={validateEmail(emailValue) ? 'error' : 'primary'}
                 label={t(
                   'settings.userManagementTab.createUser.userDetails.label.email'
                 )}
               />
             </div>
             {/* Username is not editable by non admin user */}
+            <div style={{ marginTop: '5rem' }} />
             <div data-cy="username">
-              <InputFieldOutline
+              <InputField
                 value={userValue}
-                handleChange={handleUserChange}
+                onChange={handleUserChange}
                 disabled={usernameIsDisabled}
-                validationError={false}
+                variant="primary"
                 label={t(
                   'settings.userManagementTab.createUser.userDetails.label.username'
                 )}
