47a745fcd8a9d262a15a24f2fe82e40aab0ef55d
Sayan Mondal <sayan@chaosnative.com>
2021-03-30 15:55:39 +0530

    chore(Dagre Graph): Added Basic Workflow Preview and install-experiment steps for Predefined Workflows 🖼️ (#2611)
    * 😻 Added Workflow Preview
    * 😻 Added install-experiment steps and Styled Workflow Preview
    * Adding Basic Workflow Preview
    * Updated to latest litmus-v2 and renamed variable
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- litmus-portal/frontend/src/views/CreateWorkflow/TuneWorkflow/ExtractSteps.ts
+++ litmus-portal/frontend/src/views/CreateWorkflow/TuneWorkflow/ExtractSteps.ts
@@ -25,47 +25,58 @@ export const extractSteps = (isCustom: boolean, crd: string) => {
     });
   } else {
     // Save the Pre-defined experiments Serial/Parallel
-    fetch(crd)
-      .then((data) => {
-        data.text().then((yamlText) => {
-          const parsedYaml = YAML.parse(yamlText);
+    const parsedYaml = YAML.parse(crd);
+    steps = [];
+    parsedYaml.spec.templates.forEach((template: any) => {
+      // Extracting Run Chaos Steps and appending to the array
+      if (
+        template.name === 'argowf-chaos' &&
+        template.steps[1][0].name === 'run-chaos'
+      ) {
+        // Adding install-experiment step
+        steps.push([
+          {
+            name: template.steps[0][0].name,
+            template: template.steps[0][0].name,
+          },
+        ]);
 
-          steps = [];
-
-          parsedYaml.spec.templates.forEach((template: any) => {
-            if (
-              template.name === 'argowf-chaos' &&
-              template.steps[1][0].name === 'run-chaos'
-            ) {
-              // Serialized Steps
-              parsedYaml.spec.templates.forEach((template: any) => {
-                if (template.name === 'run-chaos') {
-                  template.inputs.artifacts.forEach((artifact: any) => {
-                    const chaosEngine = YAML.parse(artifact.raw.data);
-                    if (chaosEngine.kind === 'ChaosEngine') {
-                      steps.push([
-                        {
-                          name: chaosEngine.metadata.name,
-                          template: chaosEngine.metadata.template,
-                        },
-                      ]);
-                    }
-                  });
-                }
-              });
-            } else if (
-              template.name === 'argowf-chaos' &&
-              template.steps[1][0].name !== 'run-chaos'
-            ) {
-              // Nested Steps
-              steps = template.steps as Steps[][];
-            }
-          });
+        // Serialized Steps
+        parsedYaml.spec.templates.forEach((template: any) => {
+          if (template.name === 'run-chaos') {
+            template.inputs.artifacts.forEach((artifact: any) => {
+              const chaosEngine = YAML.parse(artifact.raw.data);
+              if (chaosEngine.kind === 'ChaosEngine') {
+                steps.push([
+                  {
+                    name: chaosEngine.metadata.name,
+                    template: chaosEngine.metadata.template,
+                  },
+                ]);
+              }
+            });
+          }
         });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+
+        // Adding revert-chaos step
+        template.steps.forEach((step: any) => {
+          if (step[0].name === 'revert-chaos') {
+            steps.push([
+              {
+                name: step[0].name,
+                template: step[0].name,
+              },
+            ]);
+          }
+        });
+      } else if (
+        template.name === 'argowf-chaos' &&
+        template.steps[1][0].name !== 'run-chaos'
+      ) {
+        // Nested Steps
+        steps = template.steps as Steps[][];
+      }
+    });
   }
   return steps;
 };
