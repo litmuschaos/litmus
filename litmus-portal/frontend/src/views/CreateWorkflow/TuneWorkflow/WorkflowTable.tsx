e9b68168bd5ea667b9a594191dbd368d427766dc
Sayan Mondal <sayan@chaosnative.com>
2021-04-09 20:28:09 +0530

    chore(scheduling): Added revert chaos 👾👾 (#2677)
    * Added revert chaos
    * Adding translation
    * Adding translation fix
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- litmus-portal/frontend/src/views/CreateWorkflow/TuneWorkflow/WorkflowTable.tsx
+++ litmus-portal/frontend/src/views/CreateWorkflow/TuneWorkflow/WorkflowTable.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core';
+import { Typography, useTheme } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -6,6 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import localforage from 'localforage';
 import React, {
   forwardRef,
@@ -13,8 +14,10 @@ import React, {
   useImperativeHandle,
   useState,
 } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import YAML from 'yaml';
+import Row from '../../../containers/layouts/Row';
 import { experimentMap } from '../../../models/redux/workflow';
 import useActions from '../../../redux/actions';
 import * as WorkflowActions from '../../../redux/actions/workflow';
@@ -38,8 +41,12 @@ interface ChaosCRDTable {
 
 const WorkflowTable = forwardRef(({ isCustom }: WorkflowTableProps, ref) => {
   const classes = useStyles();
+  const { t } = useTranslation();
+
+  const theme = useTheme();
   const workflow = useActions(WorkflowActions);
   const [experiments, setExperiments] = useState<ChaosCRDTable[]>([]);
+  const [revertChaos, setRevertChaos] = useState<boolean>(false);
   const [displayStepper, setDisplayStepper] = useState<boolean>(false);
   const [engineIndex, setEngineIndex] = useState<number>(0);
   const manifest = useSelector(
@@ -84,11 +91,50 @@ const WorkflowTable = forwardRef(({ isCustom }: WorkflowTableProps, ref) => {
     setExperiments(expData);
   };
 
+  // Revert Chaos
+  const toggleRevertChaos = (manifest: string) => {
+    let deleteEngines = 'kubectl delete chaosengine ';
+    const parsedYAML = YAML.parse(manifest);
+
+    parsedYAML.spec.templates[0].steps.push([
+      {
+        name: 'revert-chaos',
+        template: 'revert-chaos',
+      },
+    ]);
+
+    parsed(manifest).forEach((engine) => {
+      deleteEngines = `${deleteEngines + engine} `;
+    });
+
+    deleteEngines += '-n {{workflow.parameters.adminModeNamespace}}';
+
+    parsedYAML.spec.templates[parsedYAML.spec.templates.length] = {
+      name: 'revert-chaos',
+      container: {
+        image: 'litmuschaos/k8s:latest',
+        command: ['sh', '-c'],
+        args: [deleteEngines],
+      },
+    };
+
+    workflow.setWorkflowManifest({
+      manifest: YAML.stringify(parsedYAML),
+    });
+  };
+
   const closeConfigurationStepper = () => {
     workflow.setWorkflowManifest({
       engineYAML: '',
     });
     setDisplayStepper(false);
+  };
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newValue: boolean
+  ) => {
+    setRevertChaos(newValue);
   };
 
   useEffect(() => {
@@ -101,6 +147,7 @@ const WorkflowTable = forwardRef(({ isCustom }: WorkflowTableProps, ref) => {
     if (experiments.length === 0) {
       return false; // Should show alert
     }
+    if (revertChaos === true) toggleRevertChaos(manifest);
     return true; // Should not show any alert
   }
 
@@ -111,54 +158,116 @@ const WorkflowTable = forwardRef(({ isCustom }: WorkflowTableProps, ref) => {
   return (
     <div>
       {!displayStepper ? (
-        <TableContainer className={classes.table} component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Sequence</TableCell>
-                <TableCell align="left">Name</TableCell>
-                <TableCell align="left">Namespace</TableCell>
-                <TableCell align="left">Application</TableCell>
-                <TableCell align="left">Probes</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {experiments.length > 0 ? (
-                experiments.map((experiment: ChaosCRDTable, index) => (
-                  <TableRow key={experiment.Name}>
-                    <TableCell component="th" scope="row">
-                      {index + 1}
-                    </TableCell>
-                    <TableCell
-                      onClick={() => {
-                        setDisplayStepper(true);
-                        setEngineIndex(experiment.StepIndex);
-                        workflow.setWorkflowManifest({
-                          engineYAML: experiment.ChaosEngine,
-                        });
-                      }}
-                      align="left"
-                      style={{ cursor: 'pointer' }}
-                    >
-                      {experiment.Name}
-                    </TableCell>
-                    <TableCell align="left">{experiment.Namespace}</TableCell>
-                    <TableCell align="left">{experiment.Application}</TableCell>
-                    <TableCell align="left">{experiment.Probes}</TableCell>
-                  </TableRow>
-                ))
-              ) : (
+        <>
+          <TableContainer className={classes.table} component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
                 <TableRow>
-                  <TableCell colSpan={5}>
-                    <Typography align="center">
-                      Please add experiments to see the data
-                    </Typography>
+                  <TableCell>
+                    {t('createWorkflow.chooseWorkflow.table.head1')}
+                  </TableCell>
+                  <TableCell align="left">
+                    {t('createWorkflow.chooseWorkflow.table.head2')}
+                  </TableCell>
+                  <TableCell align="left">
+                    {t('createWorkflow.chooseWorkflow.table.head3')}
+                  </TableCell>
+                  <TableCell align="left">
+                    {t('createWorkflow.chooseWorkflow.table.head4')}
+                  </TableCell>
+                  <TableCell align="left">
+                    {t('createWorkflow.chooseWorkflow.table.head5')}
                   </TableCell>
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {experiments.length > 0 ? (
+                  experiments.map((experiment: ChaosCRDTable, index) => (
+                    <TableRow key={experiment.Name}>
+                      <TableCell component="th" scope="row">
+                        {index + 1}
+                      </TableCell>
+                      <TableCell
+                        onClick={() => {
+                          setDisplayStepper(true);
+                          setEngineIndex(experiment.StepIndex);
+                          workflow.setWorkflowManifest({
+                            engineYAML: experiment.ChaosEngine,
+                          });
+                        }}
+                        align="left"
+                        style={{ cursor: 'pointer' }}
+                      >
+                        {experiment.Name}
+                      </TableCell>
+                      <TableCell align="left">{experiment.Namespace}</TableCell>
+                      <TableCell align="left">
+                        {experiment.Application}
+                      </TableCell>
+                      <TableCell align="left">{experiment.Probes}</TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={5}>
+                      <Typography align="center">
+                        {t('createWorkflow.chooseWorkflow.pleaseAddExp')}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {isCustom && (
+            <TableContainer className={classes.revertChaos} component={Paper}>
+              <Row className={classes.wrapper}>
+                <div className={classes.key}>
+                  <Typography>
+                    {t('createWorkflow.chooseWorkflow.revertSchedule')}
+                  </Typography>
+                </div>
+                <div>
+                  <ToggleButtonGroup
+                    value={revertChaos}
+                    exclusive
+                    onChange={handleChange}
+                    aria-label="text alignment"
+                  >
+                    <ToggleButton
+                      value
+                      style={{
+                        backgroundColor: revertChaos
+                          ? theme.palette.success.main
+                          : theme.palette.disabledBackground,
+                        color: revertChaos
+                          ? theme.palette.common.white
+                          : theme.palette.text.disabled,
+                      }}
+                      aria-label="centered"
+                    >
+                      {t('createWorkflow.chooseWorkflow.trueValue')}
+                    </ToggleButton>
+                    <ToggleButton
+                      value={false}
+                      style={{
+                        backgroundColor: !revertChaos
+                          ? theme.palette.error.main
+                          : theme.palette.disabledBackground,
+                        color: !revertChaos
+                          ? theme.palette.common.white
+                          : theme.palette.text.disabled,
+                      }}
+                      aria-label="centered"
+                    >
+                      {t('createWorkflow.chooseWorkflow.falseValue')}
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+              </Row>
+            </TableContainer>
+          )}
+        </>
       ) : (
         <ConfigurationStepper
           experimentIndex={engineIndex}
