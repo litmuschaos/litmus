c15ebd049c17cc5e97382178135c4c59c0300772
Ritik Srivastava <ritik@chaosnative.com>
2021-04-12 10:19:10 +0530

    tooltip shadow
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>

--- litmus-portal/frontend/src/components/ToolTip/index.tsx
+++ litmus-portal/frontend/src/components/ToolTip/index.tsx
@@ -6,7 +6,7 @@ const ToolTip = withStyles((theme: Theme) => ({
     backgroundColor: theme.palette.background.paper,
     color: theme.palette.text.primary,
     boxShadow:
-      '0px 2.047px 6.14px rgba(0,0,0,0.1), 0px 10.91px 24.5px rgba(0,0,0,0.13)',
+      '0rem 0.128rem 0.384rem rgba(0,0,0,0.1), 0rem 0.682rem 1.54rem rgba(0,0,0,0.13)',
     fontSize: 10,
     fontWeight: 400,
   },
