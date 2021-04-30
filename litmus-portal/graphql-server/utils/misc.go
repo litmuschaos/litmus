3cb48ea7488152a84c1d33289f68404404e54439
Ishan Gupta <ishan@chaosnative.com>
2021-04-30 21:41:05 +0530

    Minor fix for ns mode- agent scope to be taken from cluster registration request (#2758)
    * minor fix for ns mode
    
    Signed-off-by: ishangupta-ds <ishan@chaosnative.com>
    
    * minor fix
    
    Signed-off-by: ishangupta-ds <ishan@chaosnative.com>
    
    * minor fix
    
    Signed-off-by: ishangupta-ds <ishan@chaosnative.com>

--- litmus-portal/graphql-server/utils/misc.go
+++ litmus-portal/graphql-server/utils/misc.go
@@ -64,7 +64,8 @@ func ManifestParser(cluster dbSchemaCluster.Cluster, rootPath string, subscriber
 		serviceAccountStr = "---\napiVersion: v1\nkind: ServiceAccount\nmetadata:\n  name: " + ServiceAccountName + "\n  namespace: " + AgentNamespace + "\n"
 	)
 
-	if *cluster.AgentNsExists == false && os.Getenv("AGENT_SCOPE") != "namespace" {
+	// Checking if the agent namespace does not exist and its scope of installation is not namespaced
+	if *cluster.AgentNsExists == false && cluster.AgentScope != "namespace" {
 		generatedYAML = append(generatedYAML, fmt.Sprintf(namspaceStr))
 	}
 
