4a50d8884f3d46658542d712911a9977f3db450a
Amit Kumar Das <amit@chaosnative.com>
2021-05-18 13:20:59 +0530

    Minor change
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>

--- litmus-portal/frontend/src/views/ChaosWorkflows/CustomStatus/__tests__/CustomStatus.test.tsx
+++ litmus-portal/frontend/src/views/ChaosWorkflows/CustomStatus/__tests__/CustomStatus.test.tsx
@@ -11,8 +11,6 @@ describe('Custom Status', () => {
   it('Renders Succeeded Status', () => {
     const { getByTestId } = render(<CustomStatus status="Succeeded" />);
     const status = getByTestId('status');
-
-    expect(status).toBeTruthy;
     expect(status!.innerHTML).toEqual(
       'workflowDetailsView.workflowInfo.Completed'
     );
@@ -20,15 +18,11 @@ describe('Custom Status', () => {
   it('Renders Running Status', () => {
     const { getByTestId } = render(<CustomStatus status="Running" />);
     const status = getByTestId('status');
-
-    expect(status).toBeTruthy;
     expect(status!.innerHTML).toEqual('Running');
   });
   it('Renders Failed Status', () => {
     const { getByTestId } = render(<CustomStatus status="Failed" />);
     const status = getByTestId('status');
-
-    expect(status).toBeTruthy;
     expect(status!.innerHTML).toEqual('Failed');
   });
 });
