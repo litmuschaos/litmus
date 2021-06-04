a69bd9e41e6c08f1f1e5ef88f4c22210d66d6def
Hemanth Krishna <hkpdev008@gmail.com>
2021-06-04 13:19:16 +0530

    init: refactor of auth server
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>

---
+++ litmus-portal/authentication/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "litmus-portal authentication server is running",
+		})
+	})
+	r.Run()
+}
