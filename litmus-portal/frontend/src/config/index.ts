6bf768d79ef47b34db0f43e2442c30816b1fea9b
arkajyotiMukherjee <arko@chaosnative.com>
2021-06-04 16:02:29 +0530

    removed comments from config
    Signed-off-by: arkajyotiMukherjee <arko@chaosnative.com>

--- litmus-portal/frontend/src/config/index.ts
+++ litmus-portal/frontend/src/config/index.ts
@@ -11,12 +11,9 @@ if (
   process.env.NODE_ENV.trim() === 'development' ||
   process.env.NODE_ENV.trim() === 'test'
 ) {
-  // authURL = `http://a214f6f52ab5c465e9abbddb45879dc3-709983425.us-east-2.elb.amazonaws.com:9003`;
   authURL = `${window.location.protocol}//${window.location.hostname}:3000`;
   apiURL = `${window.location.protocol}//${window.location.hostname}:8080`;
   sockURL += `//${window.location.hostname}:8080`;
-  // apiURL = `http://a214f6f52ab5c465e9abbddb45879dc3-709983425.us-east-2.elb.amazonaws.com:9002`;
-  // sockURL += `//a214f6f52ab5c465e9abbddb45879dc3-709983425.us-east-2.elb.amazonaws.com:9002`;
 } else {
   authURL = '/auth';
   apiURL = '/api';
