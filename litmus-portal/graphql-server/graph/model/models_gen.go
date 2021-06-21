3b36a4c28b6094f132f1521321fd920eb20335f6
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2021-06-21 10:12:58 +0530

    added usage query for admin
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- litmus-portal/graphql-server/graph/model/models_gen.go
+++ litmus-portal/graphql-server/graph/model/models_gen.go
@@ -15,6 +15,12 @@ type ActionPayload struct {
 	ExternalData *string `json:"external_data"`
 }
 
+type AgentStat struct {
+	Ns      int `json:"Ns"`
+	Cluster int `json:"Cluster"`
+	Total   int `json:"Total"`
+}
+
 type Annotation struct {
 	Categories       string `json:"Categories"`
 	Vendor           string `json:"Vendor"`
@@ -335,6 +341,11 @@ type MemberInput struct {
 	Role      *MemberRole `json:"role"`
 }
 
+type MemberStat struct {
+	Owner *Owner `json:"Owner"`
+	Total int    `json:"Total"`
+}
+
 type Metadata struct {
 	Name        string      `json:"Name"`
 	Version     string      `json:"Version"`
@@ -375,6 +386,12 @@ type MyHubStatus struct {
 	SSHPrivateKey *string  `json:"SSHPrivateKey"`
 	SSHPublicKey  *string  `json:"SSHPublicKey"`
 	LastSyncedAt  string   `json:"LastSyncedAt"`
+}
+
+type Owner struct {
+	UserID   string `json:"UserId"`
+	Username string `json:"Username"`
+	Name     string `json:"Name"`
 }
 
 type PackageInformation struct {
@@ -422,6 +439,14 @@ type Project struct {
 	CreatedAt string    `json:"created_at"`
 	UpdatedAt string    `json:"updated_at"`
 	RemovedAt string    `json:"removed_at"`
+}
+
+type ProjectData struct {
+	Name      string        `json:"Name"`
+	Workflows *WorkflowStat `json:"Workflows"`
+	Agents    *AgentStat    `json:"Agents"`
+	ProjectID string        `json:"ProjectId"`
+	Members   *MemberStat   `json:"Members"`
 }
 
 type Provider struct {
@@ -474,6 +499,13 @@ type TemplateInput struct {
 	IsCustomWorkflow    bool   `json:"isCustomWorkflow"`
 }
 
+type TotalCount struct {
+	Projects  int           `json:"Projects"`
+	Users     int           `json:"Users"`
+	Agents    *AgentStat    `json:"Agents"`
+	Workflows *WorkflowStat `json:"Workflows"`
+}
+
 type UpdateMyHub struct {
 	ID            string   `json:"id"`
 	HubName       string   `json:"HubName"`
@@ -493,6 +525,24 @@ type UpdateUserInput struct {
 	Name        *string `json:"name"`
 	Email       *string `json:"email"`
 	CompanyName *string `json:"company_name"`
+}
+
+type UsageData struct {
+	Projects     []*ProjectData `json:"Projects"`
+	TotalEntries int            `json:"TotalEntries"`
+	TotalCount   *TotalCount    `json:"TotalCount"`
+}
+
+type UsageQuery struct {
+	Pagination    *Pagination     `json:"Pagination"`
+	DateRange     *DateRange      `json:"DateRange"`
+	Sort          *UsageSortInput `json:"Sort"`
+	SearchProject *string         `json:"SearchProject"`
+}
+
+type UsageSortInput struct {
+	Field      UsageSort `json:"Field"`
+	Descending bool      `json:"Descending"`
 }
 
 type User struct {
@@ -601,6 +651,12 @@ type WorkflowRuns struct {
 type WorkflowSortInput struct {
 	Field      WorkflowSortingField `json:"field"`
 	Descending *bool                `json:"descending"`
+}
+
+type WorkflowStat struct {
+	Schedules int `json:"Schedules"`
+	Runs      int `json:"Runs"`
+	ExpRuns   int `json:"ExpRuns"`
 }
 
 type WorkflowStats struct {
@@ -991,6 +1047,57 @@ func (e *TimeFrequency) UnmarshalGQL(v interface{}) error {
 }
 
 func (e TimeFrequency) MarshalGQL(w io.Writer) {
+	fmt.Fprint(w, strconv.Quote(e.String()))
+}
+
+type UsageSort string
+
+const (
+	UsageSortProject        UsageSort = "Project"
+	UsageSortOwner          UsageSort = "Owner"
+	UsageSortAgents         UsageSort = "Agents"
+	UsageSortSchedules      UsageSort = "Schedules"
+	UsageSortWorkflowRuns   UsageSort = "WorkflowRuns"
+	UsageSortExperimentRuns UsageSort = "ExperimentRuns"
+	UsageSortTeamMembers    UsageSort = "TeamMembers"
+)
+
+var AllUsageSort = []UsageSort{
+	UsageSortProject,
+	UsageSortOwner,
+	UsageSortAgents,
+	UsageSortSchedules,
+	UsageSortWorkflowRuns,
+	UsageSortExperimentRuns,
+	UsageSortTeamMembers,
+}
+
+func (e UsageSort) IsValid() bool {
+	switch e {
+	case UsageSortProject, UsageSortOwner, UsageSortAgents, UsageSortSchedules, UsageSortWorkflowRuns, UsageSortExperimentRuns, UsageSortTeamMembers:
+		return true
+	}
+	return false
+}
+
+func (e UsageSort) String() string {
+	return string(e)
+}
+
+func (e *UsageSort) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("enums must be strings")
+	}
+
+	*e = UsageSort(str)
+	if !e.IsValid() {
+		return fmt.Errorf("%s is not a valid UsageSort", str)
+	}
+	return nil
+}
+
+func (e UsageSort) MarshalGQL(w io.Writer) {
 	fmt.Fprint(w, strconv.Quote(e.String()))
 }
 
