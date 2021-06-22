ff4e56277fb0148d01cb0bc50ab2979c17431105
Amit Kumar Das <40661238+amityt@users.noreply.github.com>
2021-06-22 09:11:46 +0530

    chore(litmus-portal): Added overall usage and project stats page (#2921)
    * Added global stats cards
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    * Moved inline styles to styles file
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    * Making Litmus Cards Reusable and Modular
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Added project details query and basic table
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    * Adding Header for table
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Adding Search for table
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Fixed search functionality
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    * Added tarnslation for main page + Usage Cards
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Added projects table and refactoring
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    * 🔨 Changed Active SVG stroke and Fixed DeepScan 🔧
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Addsort functionality
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    * Minor fix
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>
    
    Co-authored-by: Sayan Mondal <sayan@chaosnative.com>

--- litmus-portal/frontend/src/graphql/queries.ts
+++ litmus-portal/frontend/src/graphql/queries.ts
@@ -596,3 +596,51 @@ export const GET_IMAGE_REGISTRY = gql`
     }
   }
 `;
+
+export const GET_GLOBAL_STATS = gql`
+  query getGlobalStats($query: UsageQuery!) {
+    UsageQuery(query: $query) {
+      TotalCount {
+        Workflows {
+          Runs
+          ExpRuns
+          Schedules
+        }
+        Agents {
+          Ns
+          Cluster
+          Total
+        }
+        Projects
+        Users
+      }
+    }
+  }
+`;
+
+export const GLOBAL_PROJECT_DATA = gql`
+  query getStats($query: UsageQuery!) {
+    UsageQuery(query: $query) {
+      Projects {
+        Name
+        Workflows {
+          Schedules
+          ExpRuns
+          Runs
+        }
+        Agents {
+          Total
+          Ns
+          Cluster
+        }
+        Members {
+          Owner {
+            Name
+            Username
+          }
+          Total
+        }
+      }
+    }
+  }
+`;
---
+++ litmus-portal/frontend/src/views/Usage/UsageStats/index.tsx
@@ -0,0 +1,95 @@
+import { useQuery } from '@apollo/client';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Loader from '../../../components/Loader';
+import { GET_GLOBAL_STATS } from '../../../graphql';
+import Card from './Cards';
+import useStyles from './styles';
+
+const UsageStats = () => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const { data, loading } = useQuery(GET_GLOBAL_STATS, {
+    variables: {
+      query: {
+        DateRange: {
+          start_date: Math.trunc(
+            new Date(
+              new Date().getFullYear(),
+              new Date().getMonth(),
+              1
+            ).getTime() / 1000
+          ).toString(),
+          end_date: Math.trunc(new Date().getTime() / 1000).toString(),
+        },
+      },
+    },
+  });
+
+  return (
+    <div className={classes.cardDiv}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Card
+            image="./icons/users.svg"
+            header={t('usage.card.userHeader')}
+            subtitle={t('usage.card.userSubtitle')}
+            color={classes.usersData}
+            data={data?.UsageQuery.TotalCount.Users}
+          />
+          <Card
+            image="./icons/viewProjects.svg"
+            header={t('usage.card.projectHeader')}
+            subtitle={t('usage.card.projectSubtitle')}
+            color={classes.projectData}
+            data={data?.UsageQuery.TotalCount.Projects}
+          />
+          <Card
+            image="./icons/targets.svg"
+            header={t('usage.card.agentHeader')}
+            subtitle={t('usage.card.agentSubtitle')}
+            color={classes.agentsData}
+            data={data?.UsageQuery.TotalCount.Agents.Total}
+            split
+            subData={[
+              {
+                option1: data?.UsageQuery.TotalCount.Agents.Cluster,
+                option2: `${t('usage.card.agentClusterScope')}`,
+              },
+              {
+                option1: data?.UsageQuery.TotalCount.Agents.Ns,
+                option2: `${t('usage.card.agentNamespaceScope')}`,
+              },
+            ]}
+          />
+
+          <Card
+            image="./icons/workflow-calender.svg"
+            header={t('usage.card.workflowScheduleHeader')}
+            subtitle={t('usage.card.workflowScheduleSubtitle')}
+            color={classes.schedules}
+            data={data?.UsageQuery.TotalCount.Workflows.Schedules}
+          />
+          <Card
+            image="./icons/workflows-outline.svg"
+            header={t('usage.card.workflowRunHeader')}
+            subtitle={t('usage.card.workflowRunSubtitle')}
+            color={classes.wfRuns}
+            data={data?.UsageQuery.TotalCount.Workflows.Runs}
+          />
+          <Card
+            image="./icons/myhub.svg"
+            header={t('usage.card.experimentRunHeader')}
+            subtitle={t('usage.card.experimentRunSubtitle')}
+            color={classes.expRuns}
+            data={data?.UsageQuery.TotalCount.Workflows.ExpRuns}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default UsageStats;
