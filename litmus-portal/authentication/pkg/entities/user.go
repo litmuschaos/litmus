9204af1e8d3088e8b244cc0f03727eb600e2b8c8
Hemanth Krishna <31009634+DarthBenro008@users.noreply.github.com>
2021-06-23 06:06:10 -0400

    refactor: litmus-portal authentication server (#2867)
    * init: refactor of auth server
    
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>
    
    * add: user model
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * add: db and jwt deps
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * add: user service
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: basic authentication api
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: create and update password routes
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: jwt authentication
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: admin setup and server boot validations
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: error handling and response
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * add: gin cors and release mode
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * fix: dockerfile
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * fix: route names
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * fix: prevent multiple users with same username
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: add mongo indexes
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * feat: logrus logging
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * fix: goimports and add comments for exported functions
    
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>
    
    * fix: optional email in CreateUser route
    
    Signed-off-by: Hemanth Krishna <hkpdev008@gmail.com>
    
    * feat: create handlers and sanitizers
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * lint: goimports and gofmt project
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * ref: update util configs
    
    Signed-off-by: Hemanth Krishna (DarthBenro008) <hkpdev008@gmail.com>
    
    * fix: enable multiple admins
    
    Signed-off-by: DarthBenro008 <hkpdev008@gmail.com>
    
    * feat: strict password policies
    
    Signed-off-by: DarthBenro008 <hkpdev008@gmail.com>
    
    * update: manifests to support STRICT_PASSWORD_POLICY
    
    Signed-off-by: DarthBenro008 <hkpdev008@gmail.com>

---
+++ litmus-portal/authentication/pkg/entities/user.go
@@ -0,0 +1,67 @@
+package entities
+
+import (
+	"litmus/litmus-portal/authentication/pkg/utils"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//User contains the user information
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	UserName  string             `bson:"username,omitempty" json:"username"`
+	Password  string             `bson:"password,omitempty" json:"password,omitempty"`
+	Email     string             `bson:"email,omitempty" json:"email,omitempty"`
+	Name      string             `bson:"name,omitempty" json:"name,omitempty"`
+	Role      Role               `bson:"role,omitempty" json:"role"`
+	LoggedIn  bool               `bson:"logged_in,omitempty" json:"logged_in,omitempty"`
+	CreatedAt *time.Time         `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt *time.Time         `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	RemovedAt *time.Time         `bson:"removed_at,omitempty" json:"removed_at,omitempty"`
+}
+
+//UserPassword defines structure for password related requests
+type UserPassword struct {
+	Username    string `json:"username,omitempty"`
+	OldPassword string `json:"old_password,omitempty"`
+	NewPassword string `json:"new_password,omitempty"`
+}
+
+//Role states the role of the user in the portal
+type Role string
+
+const (
+	//RoleAdmin gives the admin permissions to a user
+	RoleAdmin Role = "admin"
+
+	//RoleUser gives the normal user permissions to a user
+	RoleUser Role = "user"
+)
+
+//SanitizedUser returns the user object without sensitive information
+func (user *User) SanitizedUser() *User {
+	user.Password = ""
+	return user
+}
+
+//GetSignedJWT generates the JWT Token for the user object
+func (user *User) GetSignedJWT() (string, error) {
+
+	token := jwt.New(jwt.SigningMethodHS512)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["uid"] = user.ID.Hex()
+	claims["role"] = user.Role
+	claims["username"] = user.UserName
+	claims["exp"] = time.Now().Add(time.Minute * time.Duration(utils.JWTExpiryDuration)).Unix()
+
+	tokenString, err := token.SignedString([]byte(utils.JwtSecret))
+	if err != nil {
+		logrus.Info(err)
+		return "", err
+	}
+
+	return tokenString, nil
+}
