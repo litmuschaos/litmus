299308348e5afd77eef146944c01fcf85a891c7a
Amit Kumar Das <40661238+amityt@users.noreply.github.com>
2021-07-12 15:08:26 +0530

    Added query to fetch dashboard details from hub (#2966)
    * Added dashboard query from hub
    * Reverting myhub related change
    * Minor fix
    * Added rbac permissions
    * Fixed import issues
    
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>

--- litmus-portal/graphql-server/pkg/analytics/types.go
+++ litmus-portal/graphql-server/pkg/analytics/types.go
@@ -61,3 +61,39 @@ type PromSeriesResponse struct {
 	Series      string        `json:"series"`
 	LabelValues []*LabelValue `json:"labelValues"`
 }
+
+//Portal Dashboard Types
+type PortalDashboard struct {
+	DashboardID               string `json:"dashboardID"`
+	Name                      string `json:"name"`
+	Information               string `json:"information"`
+	ChaosEventQueryTemplate   string `json:"chaosEventQueryTemplate"`
+	ChaosVerdictQueryTemplate string `json:"chaosVerdictQueryTemplate"`
+	PanelGroupMap             []struct {
+		GroupName string   `json:"groupName"`
+		Panels    []string `json:"panels"`
+	} `json:"panelGroupMap"`
+	PanelGroups []struct {
+		PanelGroupName string `json:"panel_group_name"`
+		Panels         []struct {
+			PanelName    string `json:"panel_name"`
+			PanelOptions struct {
+				Points   bool `json:"points"`
+				Grids    bool `json:"grids"`
+				LeftAxis bool `json:"left_axis"`
+			} `json:"panel_options"`
+			YAxisLeft   string `json:"y_axis_left"`
+			YAxisRight  string `json:"y_axis_right"`
+			XAxisDown   string `json:"x_axis_down"`
+			Unit        string `json:"unit"`
+			PromQueries []struct {
+				PromQueryName string `json:"prom_query_name"`
+				Legend        string `json:"legend"`
+				Resolution    string `json:"resolution"`
+				Minstep       string `json:"minstep"`
+				Line          bool   `json:"line"`
+				CloseArea     bool   `json:"close_area"`
+			} `json:"prom_queries"`
+		} `json:"panels"`
+	} `json:"panelGroups"`
+}
