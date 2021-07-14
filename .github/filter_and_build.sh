339150646cea079912d9967f8614202e5bf195b0
VEDANT SHROTRIA <40681425+Jonsy13@users.noreply.github.com>
2021-07-14 15:58:30 +0530

    chore (Litmus-Portal) : Updated E2E GitHub Action Worflow (#2880)
    * Updated E2E workflow
    
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>
    
    * Added BUILD_IMAGE.md inside .github.
    
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>
    
    * Fixed the pipeline.
    
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>
    
    * Fixed KIND Cluster.
    
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- .github/filter_and_build.sh
+++ .github/filter_and_build.sh
@@ -3,23 +3,9 @@ set -e
 
 working_dir="litmus-portal"
 
-# Array of Image Names
-image_names=("litmusportal-frontend:ci" "litmusportal-server:ci" "litmusportal-auth-server:ci")
-
-# Array of DockerFile Paths
-dockerfile_paths=("frontend" "graphql-server/build" "authentication")
-
-# Array of directories, for which images have to be build if changed
-directory_array=("frontend" "graphql-server" "authentication")
-
+declare -A MYMAP=( [frontend]="litmusportal-frontend:ci" [graphql-server]="litmusportal-server:ci" [authentication]="litmusportal-auth-server:ci" )
 # Building the images on the basic of changes in paths
-for i in "${!directory_array[@]}"
-do
-	current_dir=$(echo "$working_dir/${directory_array[$i]}")
-	nofchanges=$(echo $changed_data | jq -r '[.[]."filename"] | join("\n")' | tr -d '"' | grep ^$current_dir | wc -l)
-	if [ $nofchanges != 0 ]
-	then 
-	docker build $current_dir -t litmuschaos/${image_names[$i]} -f $working_dir/${dockerfile_paths[$i]}/Dockerfile
-	kind load docker-image litmuschaos/${image_names[$i]} --name kind
-	fi
-done
+current_dir=$(echo "$working_dir/$directory")
+mkdir Images
+docker build $current_dir -t litmuschaos/${MYMAP[$directory]} -f $working_dir/${directory}/Dockerfile
+docker save "litmuschaos/${MYMAP[$directory]}" > Images/${directory}.tar
