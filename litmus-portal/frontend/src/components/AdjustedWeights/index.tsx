5b61c535cf733fbbbf818c3eded9caff31c729fd
Sayan Mondal <sayan@chaosnative.com>
2021-08-18 12:13:38 +0530

    Updating Icons to LitmusUI for components
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- litmus-portal/frontend/src/components/AdjustedWeights/index.tsx
+++ litmus-portal/frontend/src/components/AdjustedWeights/index.tsx
@@ -7,8 +7,6 @@ import useStyles from './styles';
 interface AdjustedWeightsProps {
   testName: string;
   testValue: number;
-  spacing: boolean;
-  icon: boolean;
 }
 
 /* Displays the details of adjusted weights including test name,
@@ -17,52 +15,15 @@ points and a progress bar for points. */
 const AdjustedWeights: React.FC<AdjustedWeightsProps> = ({
   testName,
   testValue,
-  spacing,
-  icon,
 }) => {
   const classes = useStyles();
   return (
     <div className={classes.outerDiv}>
       <div className={classes.innerDiv}>
-        {spacing ? (
-          <div className={classes.typoSpaced}>
-            {icon ? (
-              <div style={{ display: 'flex' }}>
-                <img
-                  src="./icons/experimentDetails.svg"
-                  alt="experiment details icon"
-                  className={classes.experimentIcon}
-                />
-                <Typography className={classes.typo}>
-                  {testName.split('-').map((text) => `${capitalize(text)} `)}
-                </Typography>
-              </div>
-            ) : (
-              <Typography className={classes.typo}>
-                {testName.split('-').map((text) => `${capitalize(text)} `)}
-              </Typography>
-            )}
-            <Typography className={`${classes.typo} ${classes.points}`}>
-              {testValue} points
-            </Typography>
-          </div>
-        ) : (
-          <Typography className={classes.typo}>
-            {icon ? (
-              <span>
-                <img
-                  src="./icons/experimentDetails.svg"
-                  alt="experiment details icon"
-                  className={classes.experimentIcon}
-                />{' '}
-                {testName.split('-').map((text) => `${capitalize(text)} `)}{' '}
-              </span>
-            ) : (
-              <>{testName.split('-').map((text) => `${capitalize(text)} `)}</>
-            )}
-            - <span className={classes.points}>{testValue} points</span>
-          </Typography>
-        )}
+        <Typography className={classes.typo}>
+          <>{testName.split('-').map((text) => `${capitalize(text)} `)}</>-{' '}
+          <span className={classes.points}>{testValue} points</span>
+        </Typography>
       </div>
 
       <div className={classes.progressbarDiv}>
