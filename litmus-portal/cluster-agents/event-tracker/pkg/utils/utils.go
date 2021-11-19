f96b56d813e893cfab270dc11748c6dca2cd74cc
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2021-11-19 19:39:53 +0530

    added ssl skip verify option in agents
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- litmus-portal/cluster-agents/event-tracker/pkg/utils/utils.go
+++ litmus-portal/cluster-agents/event-tracker/pkg/utils/utils.go
@@ -239,8 +239,7 @@ func SendRequest(workflowID string) (string, error) {
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
