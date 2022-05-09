cc9834869ebf1e8cd0ff1037f52b4abf0373f2e4
Amit Kumar Das <amit@chaosnative.com>
2022-05-09 22:43:42 +0530

    minor fix
    Signed-off-by: Amit Kumar Das <amit@chaosnative.com>

--- litmus-portal/graphql-server/graph/schema.resolvers.go
+++ litmus-portal/graphql-server/graph/schema.resolvers.go
@@ -6,7 +6,6 @@ package graph
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 	"strconv"
 	"time"
@@ -234,7 +233,7 @@ func (r *mutationResolver) DeleteChaosHub(ctx context.Context, projectID string,
 }
 
 func (r *mutationResolver) GitopsNotifier(ctx context.Context, clusterInfo model.ClusterIdentity, workflowID string) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	return gitOpsHandler.GitOpsNotificationHandler(ctx, clusterInfo, workflowID)
 }
 
 func (r *mutationResolver) EnableGitOps(ctx context.Context, config model.GitConfig) (bool, error) {
@@ -795,13 +794,3 @@ func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subsc
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) GitopsNotifer(ctx context.Context, clusterInfo model.ClusterIdentity, workflowID string) (string, error) {
-	return gitOpsHandler.GitOpsNotificationHandler(ctx, clusterInfo, workflowID)
-}
