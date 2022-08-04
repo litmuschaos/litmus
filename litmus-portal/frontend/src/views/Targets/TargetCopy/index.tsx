fcf317a53ecc905baee32edee774da3565d41221
Saranya-jena <saranya.jena@harness.io>
2022-08-04 12:15:23 +0530

    Updated litmusctl commands
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>

--- litmus-portal/frontend/src/views/Targets/TargetCopy/index.tsx
+++ litmus-portal/frontend/src/views/Targets/TargetCopy/index.tsx
@@ -29,7 +29,7 @@ const TargetCopy: React.FC = () => {
     setTimeout(() => setCopying(false), 3000);
   }
 
-  const command = 'litmusctl connect agent';
+  const command = 'litmusctl connect chaos-delegate';
 
   return (
     <div className={classes.root}>
