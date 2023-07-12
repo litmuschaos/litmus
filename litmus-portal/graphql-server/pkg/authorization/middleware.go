a7b7b3ab453fa18a262b1f5f17625c40d442f53b
namkyu1999 <lak9348@konkuk.ac.kr>
2023-07-12 18:53:44 +0900

    feat: add IsRevokedToken logic to graphql-server
    Signed-off-by: namkyu1999 <lak9348@konkuk.ac.kr>

--- litmus-portal/graphql-server/pkg/authorization/middleware.go
+++ litmus-portal/graphql-server/pkg/authorization/middleware.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type contextKey string
@@ -17,7 +19,7 @@ const (
 )
 
 // Middleware verifies jwt and checks if user has enough privilege to access route (no roles' info needed)
-func Middleware(handler http.Handler) gin.HandlerFunc {
+func Middleware(handler http.Handler, mongoClient *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		jwt := ""
 		auth, err := c.Request.Cookie(CookieName)
@@ -26,7 +28,11 @@ func Middleware(handler http.Handler) gin.HandlerFunc {
 		} else if c.Request.Header.Get("Authorization") != "" {
 			jwt = c.Request.Header.Get("Authorization")
 		}
-
+		if IsRevokedToken(jwt, mongoClient) {
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Error verifying JWT token: Token is revoked"))
+			return
+		}
 		ctx := context.WithValue(c.Request.Context(), AuthKey, jwt)
 		c.Request = c.Request.WithContext(ctx)
 		handler.ServeHTTP(c.Writer, c.Request)
@@ -34,7 +40,7 @@ func Middleware(handler http.Handler) gin.HandlerFunc {
 }
 
 // RestMiddlewareWithRole verifies jwt and checks if user has enough privilege to access route
-func RestMiddlewareWithRole(handler gin.HandlerFunc, roles []string) gin.HandlerFunc {
+func RestMiddlewareWithRole(handler gin.HandlerFunc, mongoClient *mongo.Client, roles []string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		jwt := ""
 		auth, err := c.Request.Cookie(CookieName)
@@ -42,6 +48,11 @@ func RestMiddlewareWithRole(handler gin.HandlerFunc, roles []string) gin.Handler
 			jwt = auth.Value
 		} else if c.Request.Header.Get("Authorization") != "" {
 			jwt = c.Request.Header.Get("Authorization")
+		}
+		if IsRevokedToken(jwt, mongoClient) {
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Error verifying JWT token: Token is revoked"))
+			return
 		}
 		user, err := UserValidateJWT(jwt)
 		if err != nil {
@@ -63,4 +74,21 @@ func RestMiddlewareWithRole(handler gin.HandlerFunc, roles []string) gin.Handler
 		c.Writer.WriteHeader(http.StatusUnauthorized)
 		return
 	}
+}
+
+// IsRevokedToken checks if the given JWT Token is revoked
+func IsRevokedToken(tokenString string, mongoClient *mongo.Client) bool {
+	collection := mongoClient.Database("auth").Collection("revoked-token")
+	result := struct {
+		Token     string `bson:"token"`
+		ExpireOn  int64  `bson:"expire_on"`
+		CreatedAt int64  `bson:"created_at"`
+	}{}
+	err := collection.FindOne(context.Background(), bson.M{
+		"token": tokenString,
+	}).Decode(&result)
+	if err != nil {
+		return false
+	}
+	return true
 }
