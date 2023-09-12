4d364cf1aef4a78b8bcdd123a0062d0c63a48536
Saranya-jena <saranya.jena@harness.io>
2023-09-12 13:26:55 +0530

    Updated  project schema fields to fix deactivate user api
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>

--- chaoscenter/authentication/pkg/services/transaction_service.go
+++ chaoscenter/authentication/pkg/services/transaction_service.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"context"
-	"strconv"
 	"time"
 
 	"github.com/litmuschaos/litmus/chaoscenter/authentication/pkg/entities"
@@ -37,10 +36,10 @@ func (a applicationService) UpdateStateTransaction(userRequest entities.UpdateUs
 			return utils.ErrUpdatingAdmin
 		}
 
-		var deactivateTime string
+		var deactivateTime int64
 
 		if *userRequest.IsDeactivate {
-			deactivateTime = strconv.FormatInt(time.Now().Unix(), 10)
+			deactivateTime = time.Now().UnixMilli()
 
 			// Checking if user is already deactivated
 			if user.DeactivatedAt != nil {
@@ -55,7 +54,7 @@ func (a applicationService) UpdateStateTransaction(userRequest entities.UpdateUs
 			return utils.ErrServerError
 		}
 		// Updating details in project collection
-		err = a.UpdateProjectState(user.ID, deactivateTime)
+		err = a.UpdateProjectState(user.ID, deactivateTime, *userRequest.IsDeactivate)
 		if err != nil {
 			log.Info(err)
 			return utils.ErrServerError
