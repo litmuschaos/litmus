ac62acfe1920cbf037d8782655b26f8c9fbb9c16
Hrishav <hrishav.kumar@harness.io>
2023-09-15 18:35:46 +0530

    fix: Fixed teaming page UI Issues (#4173)
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- chaoscenter/web/src/views/InviteNewMembers/InviteNewMemberListColumns.tsx
+++ chaoscenter/web/src/views/InviteNewMembers/InviteNewMemberListColumns.tsx
@@ -1,7 +1,7 @@
 import { Layout, Text } from '@harnessio/uicore';
 import type { Row } from 'react-table';
 import React from 'react';
-import { Color } from '@harnessio/design-system';
+import { Color, FontVariation } from '@harnessio/design-system';
 import { useStrings } from '@strings';
 import type { InviteUserDetails } from '@controllers/InviteNewMembers/types';
 
@@ -13,13 +13,15 @@ const UserName = ({ row: { original: data } }: MemberRow): React.ReactElement =>
   const { username, userID, name } = data;
   const { getString } = useStrings();
   return (
-    <Layout.Vertical>
-      <Text color={Color.BLACK}>{name ?? username}</Text>
+    <Layout.Vertical style={{ gap: '0.25rem' }}>
+      <Text font={{ variation: FontVariation.BODY }} color={Color.BLACK}>
+        {name ?? username}
+      </Text>
       <Layout.Horizontal flex={{ alignItems: 'center', justifyContent: 'flex-start' }} style={{ gap: '0.25rem' }}>
         <Text color={Color.GREY_500} font={{ size: 'small' }} lineClamp={1}>
           {getString('id')}:
         </Text>
-        <Text color={Color.GREY_500} font={{ size: 'small' }} lineClamp={1} width={100}>
+        <Text color={Color.GREY_500} font={{ size: 'small' }} lineClamp={1} style={{ maxWidth: 200 }}>
           {userID}
         </Text>
       </Layout.Horizontal>
@@ -30,11 +32,9 @@ const UserName = ({ row: { original: data } }: MemberRow): React.ReactElement =>
 const UserEmail = ({ row: { original: data } }: MemberRow): React.ReactElement => {
   const { email } = data;
   return (
-    <Layout.Vertical>
-      <Layout.Horizontal flex={{ justifyContent: 'flex-start' }} spacing="small" margin={{ bottom: 'small' }}>
-        <Text color={Color.BLACK}>{email}</Text>
-      </Layout.Horizontal>
-    </Layout.Vertical>
+    <Text font={{ variation: FontVariation.BODY }} color={Color.BLACK}>
+      {email}
+    </Text>
   );
 };
 
