7afda018bbf9997cc24c536de0f39235be90a98d
Hrishav <hrishav.kumar@harness.io>
2023-11-03 11:16:18 +0530

    chore: removed propreitary message
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- chaoscenter/web/src/components/RightSideBarV2/RightSideBarV2.tsx
+++ chaoscenter/web/src/components/RightSideBarV2/RightSideBarV2.tsx
@@ -1,10 +1,3 @@
-/*
- * Copyright 2022 Harness Inc. All rights reserved.
- * Use of this source code is governed by the PolyForm Shield 1.0.0 license
- * that can be found in the licenses directory at the root of this repository, also available at
- * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
- */
-
 import React from 'react';
 import { Container, Layout, Text } from '@harnessio/uicore';
 import { Color, FontVariation } from '@harnessio/design-system';
