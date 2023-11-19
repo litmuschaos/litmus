27de4a6d040d21c1f7f8e8046c6ecdc8b32eb50b
freedisch <freeproduc@gmail.com>
2023-11-19 18:01:20 +0100

    replace mock with Testwrapper
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- chaoscenter/web/src/views/Login/__tests__/LoginPage.test.tsx
+++ chaoscenter/web/src/views/Login/__tests__/LoginPage.test.tsx
@@ -2,26 +2,19 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { TestWrapper } from 'utils/testUtils';
 import LoginPageView from '../LoginPage';
-const mockHandleLogin = jest.fn();
-const mockGetString = jest.fn((key: string) => {
-  const strings = {
-    welcomeToLitmus: 'Welcome to Litmus',
-    LoginDescription: 'Please sign in to your account'
-  };
-  return strings[key as keyof typeof strings] || key;
-});
 
-jest.mock('@strings', () => ({
-  useStrings: () => ({
-    getString: mockGetString
-  })
-}));
+const mockHandleLogin = jest.fn();
+
 describe('LoginPageView', () => {
   test('renders correctly', () => {
-    render(<LoginPageView handleLogin={mockHandleLogin} loading={false} />);
+    render(
+      <TestWrapper>
+        <LoginPageView handleLogin={mockHandleLogin} loading={false} />
+      </TestWrapper>
+    );
 
-    expect(screen.getByText('Welcome to Litmus')).toBeInTheDocument();
     expect(screen.getByText('loginDescription')).toBeInTheDocument();
     expect(screen.getByText('Username')).toBeInTheDocument();
     expect(screen.getByText('Password')).toBeInTheDocument();
@@ -29,7 +22,11 @@ describe('LoginPageView', () => {
   });
 
   test('failed to change input', async () => {
-    render(<LoginPageView handleLogin={mockHandleLogin} loading={false} />);
+    render(
+      <TestWrapper>
+        <LoginPageView handleLogin={mockHandleLogin} loading={false} />
+      </TestWrapper>
+    );
     const usernameInput = screen.getByText('Username') as HTMLInputElement;
     const passwordInput = screen.getByText('Password') as HTMLInputElement;
 
