6640951936c7575e8703c1fd6a461616847a8bc6
Saranya Jena <saranya.jena@harness.io>
2024-03-11 14:53:43 +0530

    updated fuzz build file with the updated directory (#4502)
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>

--- chaoscenter/fuzz_build.sh
+++ chaoscenter/fuzz_build.sh
@@ -18,4 +18,4 @@ cd chaoscenter/graphql/server && go mod download
 go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/testing
 
-compile_native_go_fuzzer $(pwd)/pkg/environment/handler FuzzTestGetEnvironment test-fuzz
+compile_native_go_fuzzer $(pwd)/pkg/environment/handler/fuzz_tests FuzzTestGetEnvironment test-fuzz
