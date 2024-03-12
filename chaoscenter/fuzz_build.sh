787788ec251b25d3b9cba152dd4de6469ac5c7b1
Jonsy13 <vedant.shrotria@harness.io>
2024-03-12 19:57:39 +0530

    Added script changes to run tests dynamically
    Signed-off-by: Jonsy13 <vedant.shrotria@harness.io>

--- chaoscenter/fuzz_build.sh
+++ chaoscenter/fuzz_build.sh
@@ -16,18 +16,29 @@
 ################################################################################
 export GO_MOD_PATHS_MAPPING=( "graphql/server" "authentication" "subscriber" )
 
+cd chaoscenter
+
+export rootDir=$(pwd)
+
+echo ${rootDir}
+
 ls
 
-# for dir in "${GO_MOD_PATHS_MAPPING[@]}"; do
-#     cd chaoscenter/${dir} && go mod download && go install github.com/AdamKorcz/go-118-fuzz-build@latest && go get github.com/AdamKorcz/go-118-fuzz-build/testing && cd ${SRC}/litmus/
-#     fuzz_files=($(find "$(pwd)/${dir}" -type f -name '*_fuzz_test.go'))
-#     for file in "${fuzz_files[@]}"; do
-#         pkg=$(grep -m 1 '^package' "$file" | awk '{print $2}')
-#         package_path=$(dirname "${file%$pkg}")
-#         functionList=($(grep -o 'func Fuzz[A-Za-z0-9_]*' ${file} | awk '{print $2}'))
-#         for i in "${functionList[@]}"
-#         do
-#             compile_native_go_fuzzer ${package_path} ${i} ${i}
-#         done
-#     done
-# done
+for dir in "${GO_MOD_PATHS_MAPPING[@]}"; do
+    cd chaoscenter/${dir}
+    ls
+    go mod download
+    go install github.com/AdamKorcz/go-118-fuzz-build@latest
+    go get github.com/AdamKorcz/go-118-fuzz-build/testing
+    cd ${rootDir}
+    fuzz_files=($(find "$(pwd)/${dir}" -type f -name '*_fuzz_test.go'))
+    for file in "${fuzz_files[@]}"; do
+        pkg=$(grep -m 1 '^package' "$file" | awk '{print $2}')
+        package_path=$(dirname "${file%$pkg}")
+        functionList=($(grep -o 'func Fuzz[A-Za-z0-9_]*' ${file} | awk '{print $2}'))
+        for i in "${functionList[@]}"
+        do
+            compile_native_go_fuzzer ${package_path} ${i} ${i}
+        done
+    done
+done
