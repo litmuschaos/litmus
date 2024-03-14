7cdd4866d5f58953694bdabd9da08e28875d4297
Amit Kumar Das <amit.das@harness.io>
2024-03-14 12:02:10 +0530

    added fuzzy test for subscriber
    Signed-off-by: Amit Kumar Das <amit.das@harness.io>

--- chaoscenter/subscriber/pkg/events/utils_fuzz_test.go
+++ chaoscenter/subscriber/pkg/events/utils_fuzz_test.go
@@ -7,6 +7,8 @@ import (
 	"subscriber/pkg/types"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
 	"github.com/golang/mock/gomock"
 )
@@ -37,6 +39,53 @@ func FuzzGenerateWorkflowPayload(f *testing.F) {
 		}
 		if event == nil {
 			t.Errorf("Returned payload is nil")
+		}
+	})
+}
+
+func FuzzStrConvTime(f *testing.F) {
+
+	targetStruct := []int64{
+		12345, 54353, -12345,
+	}
+
+	for _, v := range targetStruct {
+		f.Add(v)
+	}
+
+	f.Fuzz(func(t *testing.T, data int64) {
+
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		resp := StrConvTime(data)
+		if resp == "" {
+			t.Log("result is in negative")
+		} else {
+			t.Log("converted successfully")
+		}
+	})
+}
+
+func FuzzGetExperimentStatus(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fuzzConsumer := fuzz.NewConsumer(data)
+
+		targetStruct := &struct {
+			wfEvent types.WorkflowEvent
+		}{}
+		err := fuzzConsumer.GenerateStruct(targetStruct)
+		if err != nil {
+			return
+		}
+
+		fmt.Println(targetStruct.wfEvent)
+		// Call the getExperimentStatus function
+		_, err = getExperimentStatus(targetStruct.wfEvent)
+		if err != nil {
+			assert.Equal(t, "status is invalid", "status is invalid")
+		} else {
+			t.Log("status returned")
 		}
 	})
 }
