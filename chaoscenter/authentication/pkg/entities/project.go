78b82ab1c241ae3cbee7e83ee92371d1558d74b7
aryan <aryan1bhokare@gmail.com>
2024-03-17 12:19:40 +0530

    Modified db schema of Owner.
    Signed-off-by: aryan <aryan1bhokare@gmail.com>

--- chaoscenter/authentication/pkg/entities/project.go
+++ chaoscenter/authentication/pkg/entities/project.go
@@ -12,7 +12,11 @@ type Project struct {
 type Owner struct {
 	UserID   string `bson:"user_id" json:"userID"`
 	Username string `bson:"username" json:"username"`
+	Invitation    Invitation `bson:"invitation" json:"invitation"`
+	JoinedAt      int64      `bson:"joined_at" json:"joinedAt"`
+	DeactivatedAt *int64     `bson:"deactivated_at,omitempty" json:"deactivatedAt,omitempty"`
 }
+
 type MemberStat struct {
 	Owner *[]Owner `bson:"owner" json:"owner"`
 	Total int      `bson:"total" json:"total"`
