44ae3b96518686a3f4f5d2a197cb62a830d92037
aryan <aryan1bhokare@gmail.com>
2024-03-24 12:28:43 +0530

    RBAC modification `LeaveProject`.
    Allows Owner to be able to leave the project.
    
    Signed-off-by: aryan <aryan1bhokare@gmail.com>

--- chaoscenter/authentication/pkg/validations/roles.go
+++ chaoscenter/authentication/pkg/validations/roles.go
@@ -8,7 +8,7 @@ var MutationRbacRules = map[string][]string{
 	"declineInvitation": {string(entities.RoleViewer),
 		string(entities.RoleEditor)},
 	"removeInvitation":  {string(entities.RoleOwner)},
-	"leaveProject":      {string(entities.RoleViewer), string(entities.RoleEditor)},
+	"leaveProject":      {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleEditor)},
 	"updateProjectName": {string(entities.RoleOwner)},
 	"getProject":        {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleEditor)},
 }
