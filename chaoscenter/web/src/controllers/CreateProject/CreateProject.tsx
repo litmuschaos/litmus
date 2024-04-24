e9d81ade7fa11ef24e71c19b0db168ebbab7392a
aryan <aryan1bhokare@gmail.com>
2024-04-24 11:28:08 +0530

    Some changes in `CreateProjectView`.
    Signed-off-by: aryan <aryan1bhokare@gmail.com>

--- chaoscenter/web/src/controllers/CreateProject/CreateProject.tsx
+++ chaoscenter/web/src/controllers/CreateProject/CreateProject.tsx
@@ -1,20 +1,25 @@
 import { useToaster } from '@harnessio/uicore';
 import React from 'react';
-import { useCreateProjectMutation } from '@api/auth';
+import { RefetchOptions, RefetchQueryFilters, QueryObserverResult } from '@tanstack/react-query';
+import { ListProjectsOkResponse, useCreateProjectMutation } from '@api/auth';
 import CreateProjectView from '@views/CreateProject';
 
 interface CreateProjectControllerProps {
   handleClose: () => void;
+  listProjectRefetch: <TPageData>(
+    options?: (RefetchOptions & RefetchQueryFilters<TPageData>) | undefined
+  ) => Promise<QueryObserverResult<ListProjectsOkResponse, unknown>>;
 }
 
 export default function CreateProjectController(props: CreateProjectControllerProps): React.ReactElement {
-  const { handleClose } = props;
+  const { handleClose, listProjectRefetch } = props;
   const { showSuccess } = useToaster();
   const { mutate: createProjectMutation, isLoading } = useCreateProjectMutation(
     {},
     {
       onSuccess: () => {
         showSuccess('successs');
+        listProjectRefetch();
       }
     }
   );
