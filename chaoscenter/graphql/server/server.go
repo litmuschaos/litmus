79a65cacde2922424bb90bfe8eabaf11eed5c997
Saranya Jena <saranya.jena@harness.io>
2024-07-08 13:59:43 +0530

    Added env based support for HTTPS connection (#4706)
    * Added env based support for HTTPS connection
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * resolved review comments
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * updated logic
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * fixed importd
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * added helper files
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * resolved comments
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * resolved comments
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * Update push.yml
    
    * minor changes
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * minor changes
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * minor changes
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * minor changes
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * minor changes
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * Update push.yml
    
    * updated oush.yam
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    * updated manifest
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    
    ---------
    
    Signed-off-by: Saranya-jena <saranya.jena@harness.io>
    Co-authored-by: Namkyu Park <53862866+namkyu1999@users.noreply.github.com>
    Signed-off-by: sagnik3788 <116512372+sagnik3788@users.noreply.github.com>

--- chaoscenter/graphql/server/server.go
+++ chaoscenter/graphql/server/server.go
@@ -4,6 +4,8 @@ import (
 	"regexp"
 	"strconv"
 
+	"google.golang.org/grpc/credentials"
+
 	"github.com/gin-gonic/gin"
 	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/api/middleware"
 	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/chaoshub"
@@ -35,6 +37,7 @@ import (
 	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/database/mongodb/config"
 	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/handlers"
 	pb "github.com/litmuschaos/litmus/chaoscenter/graphql/server/protos"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
@@ -100,7 +103,20 @@ func main() {
 	if err := validateVersion(); err != nil {
 		log.Fatal(err)
 	}
+
+	enableHTTPSConnection, err := strconv.ParseBool(utils.Config.EnableInternalTls)
+	if err != nil {
+		logrus.Errorf("unable to parse boolean value %v", err)
+	}
+
 	go startGRPCServer(utils.Config.RpcPort, mongodbOperator) // start GRPC serve
+	if enableHTTPSConnection {
+		if utils.Config.ServerTlsCertPath != "" && utils.Config.ServerTlsKeyPath != "" {
+			go startGRPCServerWithTLS(mongodbOperator) // start GRPC serve
+		} else {
+			log.Fatalf("Failure to start chaoscenter authentication REST server due to empty TLS cert file path and TLS key path")
+		}
+	}
 
 	srv := handler.New(generated.NewExecutableSchema(graph.NewConfig(mongodbOperator)))
 	srv.AddTransport(transport.POST{})
@@ -152,8 +168,24 @@ func main() {
 	projectEventChannel := make(chan string)
 	go projects.ProjectEvents(projectEventChannel, mongodb.MgoClient, mongodbOperator)
 
-	log.Infof("chaos manager running at http://localhost:%s", utils.Config.HttpPort)
+	if enableHTTPSConnection {
+		log.Infof("graphql server running at https://localhost:%s", utils.Config.HttpsPort)
+		// configuring TLS config based on provided certificates & keys
+		conf := utils.GetTlsConfig(utils.Config.ServerTlsCertPath, utils.Config.ServerTlsKeyPath, true)
+
+		server := http.Server{
+			Addr:      ":" + utils.Config.HttpsPort,
+			Handler:   router,
+			TLSConfig: conf,
+		}
+		if utils.Config.ServerTlsCertPath != "" && utils.Config.ServerTlsKeyPath != "" {
+			go log.Fatal(server.ListenAndServeTLS("", ""))
+		}
+	}
+
+	log.Infof("graphql server running at http://localhost:%s", utils.Config.HttpPort)
 	log.Fatal(http.ListenAndServe(":"+utils.Config.HttpPort, router))
+
 }
 
 // startGRPCServer initializes, registers services to and starts the gRPC server for RPC calls
@@ -164,6 +196,31 @@ func startGRPCServer(port string, mongodbOperator mongodb.MongoOperator) {
 	}
 
 	grpcServer := grpc.NewServer()
+
+	// Register services
+
+	pb.RegisterProjectServer(grpcServer, &projects.ProjectServer{Operator: mongodbOperator})
+
+	log.Infof("GRPC server listening on %v", lis.Addr())
+	log.Fatal(grpcServer.Serve(lis))
+}
+
+// startGRPCServerWithTLS initializes, registers services to and starts the gRPC server for RPC calls
+func startGRPCServerWithTLS(mongodbOperator mongodb.MongoOperator) {
+
+	lis, err := net.Listen("tcp", ":"+utils.Config.RpcPortHttps)
+	if err != nil {
+		log.Fatal("failed to listen: %w", err)
+	}
+
+	// configuring TLS config based on provided certificates & keys
+	conf := utils.GetTlsConfig(utils.Config.ServerTlsCertPath, utils.Config.ServerTlsKeyPath, true)
+
+	// create tls credentials
+	tlsCredentials := credentials.NewTLS(conf)
+
+	// create grpc server with tls credential
+	grpcServer := grpc.NewServer(grpc.Creds(tlsCredentials))
 
 	// Register services
 
