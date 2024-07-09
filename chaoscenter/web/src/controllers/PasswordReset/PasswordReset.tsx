7503f0d6282d498186cafc6c38deae7230fa9587
Hrishav <hrishav.kumar@harness.io>
2024-07-09 10:14:03 +0530

    feat: Updated login and update password APIs (#4751)
    * fix: Added error response in createUser API
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    * fix: added project ID to update password mutation
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    * fix: fixed failing UTs
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>
    
    ---------
    
    Signed-off-by: Hrishav <hrishav.kumar@harness.io>

--- chaoscenter/web/src/controllers/PasswordReset/PasswordReset.tsx
+++ chaoscenter/web/src/controllers/PasswordReset/PasswordReset.tsx
@@ -5,11 +5,13 @@ import PasswordResetView from '@views/PasswordReset';
 import { useGetUserQuery, useUpdatePasswordMutation } from '@api/auth';
 import { getUserDetails, setUserDetails } from '@utils';
 import { normalizePath } from '@routes/RouteDefinitions';
+import { useAppStore } from '@context';
 
 const PasswordResetController = (): React.ReactElement => {
-  const { accountID, projectID } = getUserDetails();
+  const { accountID } = getUserDetails();
   const { showSuccess, showError } = useToaster();
   const history = useHistory();
+  const { updateAppStore } = useAppStore();
 
   const { data: currentUserData, isLoading: getUserLoading } = useGetUserQuery(
     {
@@ -26,7 +28,8 @@ const PasswordResetController = (): React.ReactElement => {
       onSuccess: data => {
         setUserDetails({ isInitialLogin: false });
         showSuccess(`${data.message}`);
-        history.push(normalizePath(`/account/${accountID}/project/${projectID}/dashboard`));
+        updateAppStore({ projectID: data.projectID });
+        history.push(normalizePath(`/account/${accountID}/project/${data.projectID}/dashboard`));
       },
       onError: err => showError(err.errorDescription)
     }
