6e46d7ca7a19c53fd50fe717ded5e6feeb9f03e7
Janhavi Alekar <97527096+JanhaviAlekar@users.noreply.github.com>
2024-07-22 10:49:39 +0530

    Fixes #4673 Improvements in user management screen (#4674)
    * fix: Accept Invitation Button duplicates
    
    Signed-off-by: JanhaviAlekar <janhavialekar@gmail.com>
    
    * fix: Delete project Invite Dialog not closing on delete invite
    
    Signed-off-by: JanhaviAlekar <janhavialekar@gmail.com>
    
    * fix: Leave project Dialog not closing on leaving project
    
    Signed-off-by: JanhaviAlekar <janhavialekar@gmail.com>
    
    * fix: handling create user error with toast
    
    Signed-off-by: JanhaviAlekar <janhavialekar@gmail.com>
    
    ---------
    
    Signed-off-by: JanhaviAlekar <janhavialekar@gmail.com>
    Signed-off-by: Janhavi Alekar <97527096+JanhaviAlekar@users.noreply.github.com>
    Co-authored-by: Saranya Jena <saranya.jena@harness.io>
    Signed-off-by: andoriyaprashant <prashantandoriya@gmail.com>

--- chaoscenter/web/src/controllers/LeaveProject/LeaveProject.tsx
+++ chaoscenter/web/src/controllers/LeaveProject/LeaveProject.tsx
@@ -23,6 +23,7 @@ export default function LeaveProjectController(props: LeaveProjectControllerProp
       onSuccess: () => {
         projectsJoinedRefetch();
         getUserWithProjectsRefetch();
+        handleClose();
       }
     }
   );
