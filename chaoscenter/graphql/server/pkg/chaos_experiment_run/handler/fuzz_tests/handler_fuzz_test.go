c35b0df246a3f5b0e9f6ac71e2d10852350ed82e
weeeeestern <liz0824@naver.com>
2024-09-01 16:04:54 +0900

    test: add fuzz test
    Signed-off-by: weeeeestern <liz0824@naver.com>

--- chaoscenter/graphql/server/pkg/chaos_experiment_run/handler/fuzz_tests/handler_fuzz_test.go
+++ chaoscenter/graphql/server/pkg/chaos_experiment_run/handler/fuzz_tests/handler_fuzz_test.go
@@ -3,9 +3,9 @@ package fuzz_tests
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/chaos_experiment_run/handler"
-	chaosExperimentRunMocks "github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/chaos_experiment_run/model/mocks"
 	chaosInfraMocks "github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/chaos_infrastructure/model/mocks"
 	dbChaosExperiment "github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/database/mongodb/chaos_experiment"
 	dbChaosExperimentRun "github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/database/mongodb/chaos_experiment_run"
@@ -13,12 +13,18 @@ import (
 	dbGitOpsMocks "github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/gitops/model/mocks"
 
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
+
+	"github.com/google/uuid"
 	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/graph/model"
+	typesMocks "github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/chaos_experiment_run/model/mocks"
+	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/database/mongodb"
 	"github.com/stretchr/testify/mock"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MockServices struct {
-	ChaosExperimentRunService  *chaosExperimentRunMocks.ChaosExperimentRunService
+	ChaosExperimentRunService  *typesMocks.ChaosExperimentRunService
 	InfrastructureService      *chaosInfraMocks.InfraService
 	GitOpsService              *dbGitOpsMocks.GitOpsService
 	ChaosExperimentOperator    *dbChaosExperiment.Operator
@@ -32,7 +38,7 @@ func NewMockServices() *MockServices {
 		mongodbMockOperator        = new(dbMocks.MongoOperator)
 		infrastructureService      = new(chaosInfraMocks.InfraService)
 		gitOpsService              = new(dbGitOpsMocks.GitOpsService)
-		chaosExperimentRunService  = new(chaosExperimentRunMocks.ChaosExperimentRunService)
+		chaosExperimentRunService  = new(typesMocks.ChaosExperimentRunService)
 		chaosExperimentOperator    = dbChaosExperiment.NewChaosExperimentOperator(mongodbMockOperator)
 		chaosExperimentRunOperator = dbChaosExperimentRun.NewChaosExperimentRunOperator(mongodbMockOperator)
 	)
@@ -60,20 +66,59 @@ func FuzzGetExperimentRun(f *testing.F) {
 		fuzzConsumer := fuzz.NewConsumer(data)
 		targetStruct := &struct {
 			ProjectID       string
-			ExperimentRunID *string
-			NotifyID        *string
+			ExperimentRunID string
+			NotifyID        string
 		}{}
+
+		targetStruct.ProjectID = uuid.New().String()
+
 		err := fuzzConsumer.GenerateStruct(targetStruct)
 		if err != nil {
 			return
 		}
 
+		ctx := context.Background()
 		mockServices := NewMockServices()
-		mockServices.MongodbOperator.On("GetAggregateExperimentRuns", mock.Anything).Return(mock.Anything, nil).Once()
+		findResult := []interface{}{bson.D{
+			{Key: "experiment_run_id", Value: targetStruct.ExperimentRunID},
+			{Key: "project_id", Value: targetStruct.ProjectID},
+			{Key: "infra_id", Value: "mockInfraID"},
+			{Key: "kubernetesInfraDetails", Value: bson.A{
+				bson.D{
+					{Key: "InfraID", Value: "mockInfraID"},
+					{Key: "Name", Value: "MockInfra"},
+					{Key: "EnvironmentID", Value: "mockEnvID"},
+					{Key: "Description", Value: "Mock Infrastructure"},
+					{Key: "PlatformName", Value: "Kubernetes"},
+					{Key: "IsActive", Value: true},
+					{Key: "UpdatedAt", Value: time.Now().Unix()},
+					{Key: "CreatedAt", Value: time.Now().Unix()},
+				},
+			}},
+			{Key: "experiment", Value: bson.A{
+				bson.D{
+					{Key: "ExperimentName", Value: "MockExperiment"},
+					{Key: "ExperimentType", Value: "MockType"},
+					{Key: "Revision", Value: bson.A{
+						bson.D{
+							{Key: "RevisionID", Value: uuid.NewString()},
+							{Key: "ExperimentManifest", Value: "mockManifest"},
+							{Key: "Weightages", Value: bson.A{
+								bson.D{{Key: "FaultName", Value: "fault1"}, {Key: "Weightage", Value: 10}},
+								bson.D{{Key: "FaultName", Value: "fault2"}, {Key: "Weightage", Value: 20}},
+							}},
+						},
+					}},
+				},
+			}},
+		}}
 
-		res, err := mockServices.ChaosExperimentRunHandler.GetExperimentRun(context.Background(), targetStruct.ProjectID, targetStruct.ExperimentRunID, targetStruct.NotifyID)
+		cursor, _ := mongo.NewCursorFromDocuments(findResult, nil, nil)
+		mockServices.MongodbOperator.On("Aggregate", mock.Anything, mongodb.ChaosExperimentRunsCollection, mock.Anything, mock.Anything).Return(cursor, nil).Once()
+
+		res, err := mockServices.ChaosExperimentRunHandler.GetExperimentRun(ctx, targetStruct.ProjectID, &targetStruct.ExperimentRunID, &targetStruct.NotifyID)
 		if err != nil {
-			t.Errorf("GetExperimentRun() error = %v", err)
+			t.Errorf("ChaosExperimentRunHandler.GetExperimentRun() error = %v", err)
 			return
 		}
 		if res == nil {
@@ -95,7 +140,19 @@ func FuzzListExperimentRun(f *testing.F) {
 		}
 
 		mockServices := NewMockServices()
-		mockServices.MongodbOperator.On("GetAggregateExperimentRuns", mock.Anything).Return(mock.Anything, nil).Once()
+		findResult := []interface{}{bson.D{
+			{Key: "project_id", Value: targetStruct.ProjectID},
+			{Key: "infra_id", Value: "abc"},
+			{
+				Key: "revision", Value: []dbChaosExperiment.ExperimentRevision{
+					{
+						RevisionID: uuid.NewString(),
+					},
+				},
+			},
+		}}
+		cursor, _ := mongo.NewCursorFromDocuments(findResult, nil, nil)
+		mockServices.MongodbOperator.On("Aggregate", mock.Anything, mongodb.ChaosExperimentRunsCollection, mock.Anything, mock.Anything).Return(cursor, nil).Once()
 
 		res, err := mockServices.ChaosExperimentRunHandler.ListExperimentRun(targetStruct.ProjectID, targetStruct.Request)
 		if err != nil {
@@ -105,6 +162,7 @@ func FuzzListExperimentRun(f *testing.F) {
 		if res == nil {
 			t.Errorf("Returned response is nil")
 		}
+
 	})
 }
 
@@ -122,6 +180,9 @@ func FuzzRunChaosWorkFlow(f *testing.F) {
 
 		mockServices := NewMockServices()
 		mockServices.MongodbOperator.On("StartSession").Return(mock.Anything, nil).Once()
+		mockServices.MongodbOperator.On("Update", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil).Once()
+		mockServices.MongodbOperator.On("CommitTransaction", mock.Anything).Return(nil).Once()
+		mockServices.MongodbOperator.On("AbortTransaction", mock.Anything).Return(nil).Once()
 
 		res, err := mockServices.ChaosExperimentRunHandler.RunChaosWorkFlow(context.Background(), targetStruct.ProjectID, targetStruct.Workflow, nil)
 		if err != nil {
@@ -136,14 +197,22 @@ func FuzzRunChaosWorkFlow(f *testing.F) {
 
 func FuzzGetExperimentRunStats(f *testing.F) {
 	f.Fuzz(func(t *testing.T, data []byte) {
-		fuzzConsumer := fuzz.NewConsumer(data)
-		projectID, err := fuzzConsumer.GetString()
-		if err != nil {
-			return
-		}
-
+		projectID := uuid.New().String()
 		mockServices := NewMockServices()
-		mockServices.MongodbOperator.On("GetAggregateExperimentRuns", mock.Anything).Return(mock.Anything, nil).Once()
+
+		findResult := []interface{}{bson.D{
+			{Key: "project_id", Value: projectID},
+			{Key: "infra_id", Value: "abc"},
+			{
+				Key: "revision", Value: []dbChaosExperiment.ExperimentRevision{
+					{
+						RevisionID: uuid.NewString(),
+					},
+				},
+			},
+		}}
+		cursor, _ := mongo.NewCursorFromDocuments(findResult, nil, nil)
+		mockServices.MongodbOperator.On("Aggregate", mock.Anything, mongodb.ChaosExperimentRunsCollection, mock.Anything, mock.Anything).Return(cursor, nil).Once()
 
 		res, err := mockServices.ChaosExperimentRunHandler.GetExperimentRunStats(context.Background(), projectID)
 		if err != nil {
