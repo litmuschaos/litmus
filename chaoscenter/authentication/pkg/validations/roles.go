4508a9fad16f7c90697c70b343ecdd39bcfe4863
Calvin Audier <calvin.audier@amadeus.com>
2025-11-24 17:31:10 +0100

    Add executor role in authentication
    Signed-off-by: Calvin Audier <calvin.audier@amadeus.com>

--- chaoscenter/authentication/pkg/validations/roles.go
+++ chaoscenter/authentication/pkg/validations/roles.go
@@ -4,13 +4,15 @@ import "github.com/litmuschaos/litmus/chaoscenter/authentication/pkg/entities"
 
 var MutationRbacRules = map[string][]string{
 	"sendInvitation":   {string(entities.RoleOwner)},
-	"acceptInvitation": {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor)},
+	"acceptInvitation": {string(entities.RoleOwner), string(entities.RoleViewer),
+		string(entities.RoleExecutor), string(entities.RoleEditor)},
 	"declineInvitation": {string(entities.RoleOwner), string(entities.RoleViewer),
-		string(entities.RoleExecutor)},
+		string(entities.RoleExecutor), string(entities.RoleEditor)},
 	"removeInvitation":  {string(entities.RoleOwner)},
-	"leaveProject":      {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor)},
+	"leaveProject":      {string(entities.RoleOwner), string(entities.RoleViewer),
+		string(entities.RoleExecutor), string(entities.RoleEditor)},
 	"updateProjectName": {string(entities.RoleOwner)},
 	"updateMemberRole":  {string(entities.RoleOwner)},
 	"deleteProject":     {string(entities.RoleOwner)},
-	"getProject":        {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor)},
+	"getProject":        {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor), string(entities.RoleEditor)},
 }
