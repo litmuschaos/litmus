b69cad3352598424abe8f6fe145c3c8cc68d82ce
akhilmukkara <akhil.mukkara@gmail.com>
2026-01-26 13:28:46 +0530

    fix: correct import order in service.go
    Fixed Go import ordering to pass goimports linting check.
    
    Signed-off-by: Akhil Mukkara <akhil.mukkara@gmail.com>
    Signed-off-by: akhilmukkara <akhil.mukkara@gmail.com>

--- chaoscenter/graphql/server/pkg/chaos_infrastructure/service.go
+++ chaoscenter/graphql/server/pkg/chaos_infrastructure/service.go
@@ -57,16 +57,16 @@ type Service interface {
 }
 
 type infraService struct {
-	infraOperator *dbChaosInfra.Operator
-	envOperator   *dbEnvironments.Operator
+	infraOperator   *dbChaosInfra.Operator
+	envOperator     *dbEnvironments.Operator
 	mongodbOperator mongodb.MongoOperator
 }
 
 // NewChaosInfrastructureService returns a new instance of Service
 func NewChaosInfrastructureService(infraOperator *dbChaosInfra.Operator, envOperator *dbEnvironments.Operator, mongodbOperator mongodb.MongoOperator) Service {
 	return &infraService{
-		infraOperator: infraOperator,
-		envOperator:   envOperator,
+		infraOperator:   infraOperator,
+		envOperator:     envOperator,
 		mongodbOperator: mongodbOperator,
 	}
 }
