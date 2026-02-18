e1a334b5204fe14a52bbb566cdd4a254307c09b8
Avnish Jaltare <avnishjaltare8@gmail.com>
2026-02-18 18:24:30 +0000

    fix: return 503 Service Unavailable when database is down
    Signed-off-by: Avnish Jaltare <avnishjaltare8@gmail.com>

--- chaoscenter/graphql/server/pkg/handlers/status_handler.go
+++ chaoscenter/graphql/server/pkg/handlers/status_handler.go
@@ -1,9 +1,13 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/litmuschaos/litmus/chaoscenter/graphql/server/pkg/database/mongodb"
 )
 
 type APIStatus struct {
@@ -12,6 +16,16 @@ type APIStatus struct {
 
 func StatusHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// Check MongoDB connection health with a short timeout
+		ctx, cancel := context.WithTimeout(c.Request.Context(), 2*time.Second)
+		defer cancel()
+
+		_, err := mongodb.Operator.ListDataBase(ctx, mongodb.MgoClient)
+		if err != nil {
+			c.JSON(http.StatusServiceUnavailable, APIStatus{Status: "down"})
+			return
+		}
+
 		c.JSON(http.StatusOK, APIStatus{Status: "up"})
 	}
 }
