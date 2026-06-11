643f2f52752c75c77c05ada4ac5b9a63926fcfa5
Calvin Audier <calvin.audier@amadeus.com>
2026-06-11 09:50:21 +0200

    Remove whitespace
    Signed-off-by: Calvin Audier <calvin.audier@amadeus.com>

--- chaoscenter/authentication/pkg/validations/roles.go
+++ chaoscenter/authentication/pkg/validations/roles.go
@@ -4,7 +4,7 @@ import "github.com/litmuschaos/litmus/chaoscenter/authentication/pkg/entities"
 
 var MutationRbacRules = map[string][]string{
 	"sendInvitation":          {string(entities.RoleOwner)},
-	"acceptInvitation":        {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor), string(entities.RoleEditor)}, 
+	"acceptInvitation":        {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor), string(entities.RoleEditor)},
 	"declineInvitation":       {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor), string(entities.RoleEditor)},
 	"removeInvitation":        {string(entities.RoleOwner)},
 	"leaveProject":            {string(entities.RoleOwner), string(entities.RoleViewer), string(entities.RoleExecutor), string(entities.RoleEditor)},
