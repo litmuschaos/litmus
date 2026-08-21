530deea108bbb54d71b2580a6372aad35b07278f
barbierajput378-pixel <barbierajput378@gmail.com>
2026-08-21 17:10:34 +0530

    chore: update types.ts
    Signed-off-by: barbierajput378-pixel <barbierajput378@gmail.com>

--- chaoscenter/web/src/strings/types.ts
+++ chaoscenter/web/src/strings/types.ts
@@ -114,7 +114,6 @@ export interface StringsMap {
   'chaosInfrastructureSuccess': unknown
   'chaosInfrastructureText': unknown
   'chaosInfrastructures': unknown
-  'clear': unknown
   'chaosResultNotAvailable': unknown
   'chaosResults': unknown
   'chaosStudio': unknown
@@ -128,6 +127,7 @@ export interface StringsMap {
   'chooseHubAccess': unknown
   'chooseMembersAddTheProject': unknown
   'chooseProbeMode': unknown
+  'clear': unknown
   'clearSearch': unknown
   'clickDownload': unknown
   'clone': unknown
@@ -156,9 +156,9 @@ export interface StringsMap {
   'confirmText': unknown
   'connectChaosHub': unknown
   'connectHub': unknown
-  'connected': unknown
   'connectInfraKubernetes': unknown
   'connectInfraTooltip': unknown
+  'connected': unknown
   'connectionModal.method': unknown
   'connectionModal.options.option1.icon': unknown
   'connectionModal.options.option1.text': unknown
@@ -204,10 +204,10 @@ export interface StringsMap {
   'customValues': unknown
   'daily': unknown
   'dailyMessage': unknown
+  'data': unknown
   'dataTypeFloat': unknown
   'dataTypeInt': unknown
   'dataTypeString': unknown
-  'data': unknown
   'dateExecuted': unknown
   'dayOfMonthLabel': unknown
   'dayOfWeekLabel': unknown
@@ -425,9 +425,9 @@ export interface StringsMap {
   'inactiveTooltip': unknown
   'infraName': unknown
   'infraNameValidationLowercase': unknown
-  'infraNameValidationNoStartDash': unknown
-  'infraNameValidationNoEndDash': unknown
   'infraNameValidationMaxLength': unknown
+  'infraNameValidationNoEndDash': unknown
+  'infraNameValidationNoStartDash': unknown
   'infraNameValidationRequired': unknown
   'infrastructure': unknown
   'infrastructureCommand': unknown
@@ -744,11 +744,11 @@ export interface StringsMap {
   'probeValidation.url': unknown
   'probeValidation.value': unknown
   'probeValidation.version': unknown
-  'production': unknown
   'probes': unknown
   'probesArePassed': unknown
   'problems': unknown
   'prod': unknown
+  'production': unknown
   'project': unknown
   'projectCreatedByYou': unknown
   'projectDashboard.noProjectDescription': unknown
@@ -766,8 +766,8 @@ export interface StringsMap {
   'promDescription': unknown
   'promProbeDetails': unknown
   'promProbeTitle': unknown
-  'prometheusEndpoint': unknown
   'prometheusDataComparison': unknown
+  'prometheusEndpoint': unknown
   'prometheusQuery': unknown
   'prometheusQueryPath': unknown
   'properties': unknown
